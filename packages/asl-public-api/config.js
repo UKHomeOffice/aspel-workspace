d9861d4835668315227f241b054606078774d691
Leonard Martin <leonard.martin@gmail.com>
2018-02-15 13:30:03 +0000

    Use dynamic database config from kube secrets

--- packages/asl-public-api/config.js
+++ packages/asl-public-api/config.js
@@ -6,5 +6,11 @@ module.exports = {
     client: process.env.KEYCLOAK_CLIENT,
     secret: process.env.KEYCLOAK_SECRET
   },
-  db: process.env.POSTGRES_URL
+  db: {
+    database: process.env.DATABASE_NAME,
+    host: process.env.DATABASE_HOST,
+    password: process.env.DATABASE_PASSWORD,
+    port: process.env.DATABASE_PORT,
+    username: process.env.DATABASE_USERNAME
+  }
 };
