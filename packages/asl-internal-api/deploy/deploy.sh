7f0a0f72b9d36746456e5e489de774fe6e271de3
Leonard Martin <leonard.martin@gmail.com>
2018-03-05 14:12:59 +0000

    Remove redundant paramter from deploy file

--- packages/asl-internal-api/deploy/deploy.sh
+++ packages/asl-internal-api/deploy/deploy.sh
@@ -3,8 +3,6 @@ export KUBE_NAMESPACE=asl-dev
 export KUBE_SERVER=${KUBE_SERVER}
 export KUBE_TOKEN=${KUBE_TOKEN}
 
-export SERVICE_NAME=inspector-api
-
 kd --insecure-skip-tls-verify \
   -f deploy/deployment.yaml \
   -f deploy/service.yaml \
