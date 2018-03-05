5119c5a390deeba001d3f973d55372f998a749b2
Leonard Martin <leonard.martin@gmail.com>
2018-03-05 12:15:22 +0000

    Simplify deployment
    Remove kube yaml files and replace with generation

--- packages/asl-public-api/deploy/deploy.sh
+++ packages/asl-public-api/deploy/deploy.sh
@@ -3,10 +3,8 @@ export KUBE_NAMESPACE=asl-dev
 export KUBE_SERVER=${KUBE_SERVER}
 export KUBE_TOKEN=${KUBE_TOKEN}
 
-export SERVICE_NAME=public-api
-
 kd --insecure-skip-tls-verify \
-  -f deploy/app-deployment.yaml \
-  -f deploy/app-service.yaml \
-  -f deploy/app-ingress.yaml \
+  -f deploy/deployment.yaml \
+  -f deploy/service.yaml \
+  -f deploy/ingress.yaml \
   -f deploy/network-policy.yaml
