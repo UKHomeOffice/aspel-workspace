850f6a7ddc67dd8978d2f2519b1294949e4cdb83
Joe Fitter <joefitter@hotmail.com>
2018-06-08 10:29:52 +0100

    Added get route for place
    * Get a single place by id
    * Include nacwo role and assoc profile

--- packages/asl-public-api/lib/routers/places.js
+++ packages/asl-public-api/lib/routers/places.js
@@ -2,6 +2,31 @@ const { Router } = require('express');
 
 const router = Router({ mergeParams: true });
 
+router.get('/:id', (req, res, next) => {
+  const { Role, Place, Profile } = req.models;
+  Promise.resolve()
+    .then(() => {
+      return Place.findOne({
+        where: {
+          id: req.params.id,
+          establishmentId: req.establishment.id
+        },
+        include: {
+          model: Role,
+          as: 'nacwo',
+          include: {
+            model: Profile
+          }
+        }
+      });
+    })
+    .then(place => {
+      res.response = place;
+      next();
+    })
+    .catch(next);
+});
+
 router.get('/', (req, res, next) => {
   const { Role, Profile } = req.models;
   Promise.resolve()
