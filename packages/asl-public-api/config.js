c576611a6170c74187cbe071d338ecb03c5ac3de
Leonard Martin <leonard.martin@gmail.com>
2018-07-31 17:04:07 +0100

    Fix database config

--- packages/asl-public-api/config.js
+++ packages/asl-public-api/config.js
@@ -13,6 +13,6 @@ module.exports = {
     host: process.env.DATABASE_HOST,
     password: process.env.DATABASE_PASSWORD,
     port: process.env.DATABASE_PORT,
-    username: process.env.DATABASE_USERNAME
+    user: process.env.DATABASE_USERNAME
   }
 };
