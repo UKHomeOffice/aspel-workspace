6dbff9baa19e1e0e21924bd54076954cb6f86a76
Elvin Ali <ali.elvin@gmail.com>
2018-08-08 15:53:08 +0100

    Add express-limiter

--- packages/asl-public-api/lib/api.js
+++ packages/asl-public-api/lib/api.js
@@ -7,9 +7,11 @@ const Workflow = require('./workflow/client');
 
 const errorHandler = require('./error-handler');
 
+const redisClient = require('redis').createClient();
+
 module.exports = settings => {
   const app = api(settings);
-
+  const limiter = require('express-limiter')(app, redisClient);
   const models = db(settings.db);
 
   app.db = models;
@@ -25,8 +27,11 @@ module.exports = settings => {
     res.meta = {};
     next();
   });
-
-  app.use('/me', require('./routers/user'));
+  app.use('/me', require('./routers/user'), limiter({
+    lookup: ['connection.remoteAddress'],
+    total: 1,
+    expire: 1000 * 60 * 60
+  }));
 
   app.use('/establishment(s)?', require('./routers/establishment'));
 
