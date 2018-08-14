f5a0b63259e6db2b6da62c661a0820801cbc8615
Leonard Martin <leonard.martin@gmail.com>
2018-08-14 11:10:51 +0100

    Add regression tests for schema searching bug
    Fixed in `schema@3.2.1` there was a bug where a match in the licence number or licence holder name would cause previous query clauses to be ignored.

--- packages/asl-public-api/test/specs/index.js
+++ packages/asl-public-api/test/specs/index.js
@@ -450,6 +450,20 @@ describe('API', () => {
 
     });
 
+    describe('/projects', () => {
+
+      it('returns only the current establishments projects when searching - bugfix', () => {
+        return request(this.api)
+          .get('/establishment/100/projects?search=abc') // "abc" matches licence number for all projects
+          .expect(200)
+          .expect(response => {
+            assert.equal(response.body.data.length, 1, 'Returns exactly one project');
+            assert.equal(response.body.data[0].title, 'Test project 1');
+          });
+      });
+
+    });
+
   });
 
 });
