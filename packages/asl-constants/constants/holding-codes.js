3078caed0616bc48fbc10ea5c297335a7b0bb4dc
Phil Moorhouse <phil@wheelsandcogs.co.uk>
2018-08-15 17:23:59 +0100

    added initial constants from schema

---
+++ packages/asl-constants/constants/holding-codes.js
@@ -0,0 +1,7 @@
+module.exports = [
+  'STH',
+  'LTH',
+  'NOH',
+  'SEP',
+  'NSEP'
+];
---
+++ packages/asl-constants/index.js
@@ -0,0 +1,21 @@
+const authorisationTypes = require('./constants/authorisation-types');
+const establishmentCountries = require('./constants/establishment-countries');
+const establishmentStatuses = require('./constants/establishment-statuses');
+const externalPermissions = require('./constants/external-permissions');
+const holdingCodes = require('./constants/holding-codes');
+const pilStatuses = require('./constants/pil-statuses');
+const projectStatuses = require('./constants/project-statuses');
+const roles = require('./constants/roles');
+const suitabilityCodes = require('./constants/suitability-codes');
+
+module.exports = {
+  authorisationTypes,
+  establishmentCountries,
+  establishmentStatuses,
+  externalPermissions,
+  holdingCodes,
+  pilStatuses,
+  projectStatuses,
+  roles,
+  suitabilityCodes
+};
