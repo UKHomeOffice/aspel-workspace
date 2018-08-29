fd7a0a514395dc29afc13296097146967536fb72
Joe Fitter <joefitter@hotmail.com>
2018-08-29 10:26:00 +0100

    use shared validate middlware

--- packages/asl-public-api/lib/routers/invite-user.js
+++ packages/asl-public-api/lib/routers/invite-user.js
@@ -30,41 +30,13 @@ const validateProfile = (req, res, next) => {
 
 const validateInvitation = (req, res, next) => {
   return validateSchema(req.models.Invitation, {
+    // pass dummy UUID as the profile has not been created at this point.
+    // profileId is added, and will be validated at time of insertion
     profileId: 'e564bf87-319c-4d5b-a266-d70cda895a30',
     establishmentId: req.establishment.id,
     role: req.body.role
   })(req, res, next);
 };
-
-// const validateSchema = () => {
-//   return (req, res, next) => {
-//     const { Invitation, Profile } = req.models;
-//
-//     const invitationProps = {
-//       // pass dummy UUID as the profile has not been created at this point.
-//       // profileId is added, and will be validated at time of insertion
-//       profileId: 'e564bf87-319c-4d5b-a266-d70cda895a30',
-//       establishmentId: req.establishment.id,
-//       role: req.body.role
-//     };
-//
-//     let error = Invitation.validate(invitationProps);
-//
-//     if (error) {
-//       return next(error);
-//     }
-//
-//     const profileProps = pick(req.body, ['firstName', 'lastName', 'email']);
-//
-//     error = Profile.validate(profileProps);
-//
-//     if (error) {
-//       return next(error);
-//     }
-//
-//     return next();
-//   };
-// };
 
 router.post('/',
   permissions('profile.invite'),
