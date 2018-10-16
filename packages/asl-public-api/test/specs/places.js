aeebebe729941886cb5d0672f6368c2c81fbacd0
Elvin Ali <ali.elvin@gmail.com>
2018-10-16 10:49:28 +0100

    Fix tests

--- packages/asl-public-api/test/specs/places.js
+++ packages/asl-public-api/test/specs/places.js
@@ -84,10 +84,14 @@ describe('/places', () => {
 
   it('sends a message to Workflow on POST', () => {
     const input = {
-      site: 'Lunar House 3rd floor',
-      name: '83',
-      suitability: ['LA', 'DOG'],
-      holding: ['NOH']
+      data:
+      {
+        site: 'Lunar House 3rd floor',
+        name: '83',
+        suitability: ['LA', 'DOG'],
+        holding: ['NOH']
+
+      }
     };
     return request(this.api)
       .post('/establishment/100/places')
@@ -100,17 +104,25 @@ describe('/places', () => {
         assert.equal(req.method, 'POST');
         assert.equal(body.model, 'place');
         assert.equal(body.action, 'create');
-        assert.deepEqual(body.data, { ...input, establishmentId: '100' });
+        assert.deepEqual(body.data, { ...input.data, establishmentId: '100' });
       });
   });
 
   it('can pass comments property without failing validation', () => {
     const input = {
-      site: 'Lunar House 3rd floor',
-      name: '83',
-      suitability: ['LA', 'DOG'],
-      holding: ['NOH'],
-      comments: 'this should not fail validation'
+      data:
+      {
+        site: 'Lunar House 3rd floor',
+        name: '83',
+        suitability: ['LA', 'DOG'],
+        holding: ['NOH']
+
+      },
+      meta:
+      {
+        changesToRestrictions: 'changes',
+        comments: 'comments'
+      }
     };
     return request(this.api)
       .post('/establishment/100/places')
@@ -120,12 +132,19 @@ describe('/places', () => {
 
   it('returns 400 for invalid or missing data', () => {
     const input = {
-      // requires "name"
-      site: 'Lunar House 3rd floor',
-      suitability: ['LA', 'DOG'],
-      holding: ['NOH']
+      data:
+      {
+        site: 'Lunar House 3rd floor',
+        // requires name
+        suitability: ['LA', 'DOG'],
+        holding: ['NOH']
+      },
+      meta:
+      {
+        changesToRestrictions: 'changes',
+        comments: 'comments'
+      }
     };
-
     return request(this.api)
       .post('/establishment/100/places')
       .send(input)
@@ -148,10 +167,18 @@ describe('/places', () => {
 
     it('returns 400 for invalid or missing data', () => {
       const input = {
-        site: 'Lunar House 3rd floor',
-        name: '83',
-        suitability: ['FAKE'],
-        holding: ['NOH']
+        data:
+        {
+          site: 'Lunar House 3rd floor',
+          name: '83',
+          suitability: ['FAKE'],
+          holding: ['NOH']
+        },
+        meta:
+        {
+          changesToRestrictions: 'changes',
+          comments: 'comments'
+        }
       };
       return request(this.api)
         .put('/establishment/100/places/1d6c5bb4-be60-40fd-97a8-b29ffaa2135f')
@@ -161,11 +188,16 @@ describe('/places', () => {
 
     it('adds a message to SQS on PUT', () => {
       const input = {
-        site: 'Lunar House 3rd floor',
-        name: '83',
-        suitability: ['LA', 'DOG'],
-        holding: ['NOH']
+        data:
+        {
+          site: 'Lunar House 3rd floor',
+          name: '83',
+          suitability: ['LA', 'DOG'],
+          holding: ['NOH']
+
+        }
       };
+
       return request(this.api)
         .put('/establishment/100/places/1d6c5bb4-be60-40fd-97a8-b29ffaa2135f')
         .send(input)
@@ -178,14 +210,12 @@ describe('/places', () => {
           assert.equal(body.model, 'place');
           assert.equal(body.action, 'update');
           assert.equal(body.id, '1d6c5bb4-be60-40fd-97a8-b29ffaa2135f');
-          assert.deepEqual(body.data, { ...input, establishmentId: '100' });
+          assert.deepEqual(body.data, { ...input.data, establishmentId: '100' });
         });
     });
 
     it('adds a message to SQS on DELETE', () => {
-      const input = {
-        comments: 'Lorem ipsum dolor'
-      };
+      const input = {};
       return request(this.api)
         .delete('/establishment/100/places/1d6c5bb4-be60-40fd-97a8-b29ffaa2135f')
         .send(input)
