eadaeb61a9c0e39ff9e92bf4eda4f02fbc154df9
Elvin Ali <ali.elvin@gmail.com>
2018-11-12 17:27:05 +0000

    Return total count of cases

--- packages/asl-public-api/lib/routers/tasks/index.js
+++ packages/asl-public-api/lib/routers/tasks/index.js
@@ -8,16 +8,15 @@ const fetchTasks = () => {
       query: {
         data: {
           subject: req.user.profile.id
-        }
+        },
+        ...req.query
       }
     };
 
-    console.log('fetch tasks params: ', params);
-
     return req.workflow(params)
-      .then(response => {
+      .then((response) => {
+        res.meta.count = response.json.total;
         res.response = response.json.data;
-        res.meta.count = response.json.data.length;
         next();
       })
       .catch(next);
