e7f4ccae0f88e9dc4153b2d13b2fc3cab5bb305b
Phil Moorhouse <phil@wheelsandcogs.co.uk>
2019-01-03 12:12:52 +0000

    remove redundant code

--- packages/asl-internal-api/lib/routers/search/profile.js
+++ packages/asl-internal-api/lib/routers/search/profile.js
@@ -4,25 +4,17 @@ module.exports = () => {
 
   const router = Router({ mergeParams: true });
 
-  const getFilterOptions = (Profile, { query }) => {
-    query = query || Profile.query();
-    return query;
-  };
-
-  const count = (Profile, { query }) => {
-    query = query || Profile.query();
-    return query.count().then(result => result[0].count);
+  const count = (Profile) => {
+    return Profile.query().count().then(result => result[0].count);
   };
 
   const searchAndFilter = (Profile, {
-    query,
     search,
-    filters = {},
     limit,
     offset,
     sort = {}
   }) => {
-    query = query || Profile.query();
+    let query = Profile.query();
 
     query
       .distinct('profiles.*')
@@ -48,28 +40,25 @@ module.exports = () => {
 
   const getAllProfiles = req => {
     const { Profile } = req.models;
-    const { search, sort, filters, limit, offset } = req.query;
+    const { search, sort, limit, offset } = req.query;
 
     const params = {
       search,
       limit,
       offset,
-      sort,
-      filters
+      sort
     };
 
     return Promise.all([
-      getFilterOptions(Profile, params),
-      count(Profile, params),
+      count(Profile),
       searchAndFilter(Profile, params)
-    ]).then(([filters, total, profiles]) => ({ filters, total, profiles }));
+    ]).then(([total, profiles]) => ({ total, profiles }));
   };
 
   router.get('/', (req, res, next) => {
     Promise.resolve()
       .then(() => getAllProfiles(req))
-      .then(({ filters, total, profiles }) => {
-        res.meta.filters = filters;
+      .then(({ total, profiles }) => {
         res.meta.total = total;
         res.meta.count = profiles.total;
         res.response = profiles.results;
