387acd2cc55c0ec14d805741d8dd75f2c64ce71c
Phil Moorhouse <phil@wheelsandcogs.co.uk>
2019-01-03 10:30:22 +0000

    add people search router

--- packages/asl-internal-api/lib/api.js
+++ packages/asl-internal-api/lib/api.js
@@ -1,6 +1,6 @@
+const proxy = require('http-proxy-middleware');
 const api = require('@asl/service/api');
 const db = require('@asl/schema');
-const proxy = require('http-proxy-middleware');
 
 const user = require('./middleware/user');
 const profile = require('./routers/profile');
@@ -8,7 +8,6 @@ const profile = require('./routers/profile');
 module.exports = settings => {
 
   const app = api(settings);
-
   const models = db(settings.db);
 
   app.use((req, res, next) => {
@@ -26,6 +25,33 @@ module.exports = settings => {
         data: res.response
       };
       response.meta = Object.assign({}, res.meta);
+      return res.json(response);
+    }
+    next();
+  });
+
+  app.use((req, res, next) => {
+    res.meta = {};
+    next();
+  });
+
+  app.use('/search/profiles', require('./routers/search/profile'));
+  // app.use('/search/establishments', require('./routers/search/establishment'));
+  // app.use('/search/projects', require('./routers/search/project'));
+
+  app.use((req, res, next) => {
+    if (res.response) {
+      const response = {
+        data: res.response
+      };
+      response.meta = Object.assign({}, res.meta);
+      if (req.establishment) {
+        response.meta.establishment = {
+          id: req.establishment.id,
+          name: req.establishment.name
+        };
+      }
+
       return res.json(response);
     }
     next();
---
+++ packages/asl-internal-api/lib/routers/search/profile.js
@@ -0,0 +1,85 @@
+const { Router } = require('express');
+
+const router = Router({ mergeParams: true });
+
+const getFilterOptions = (Profile, { query }) => {
+  query = query || Profile.query();
+  return query;
+};
+
+const count = (Profile, { query }) => {
+  query = query || Profile.query();
+  return query.count().then(result => result[0].count);
+};
+
+const searchAndFilter = (Profile, {
+  query,
+  search,
+  filters = {},
+  limit,
+  offset,
+  sort = {}
+}) => {
+  query = query || Profile.query();
+
+  query
+    .distinct('profiles.*')
+    .leftJoinRelation('[pil, projects]')
+    .eager('[pil, projects, establishments]')
+    .where(builder => {
+      if (search) {
+        return builder
+          .orWhere(builder => Profile.searchFullName({ search, query: builder }));
+      }
+    });
+
+  query = Profile.paginate({ query, limit, offset });
+
+  if (sort.column) {
+    query = Profile.orderBy({ query, sort });
+  } else {
+    query.orderBy('lastName');
+  }
+
+  return query;
+};
+
+const getAllProfiles = req => {
+  const { Profile } = req.models;
+  const { search, sort, filters, limit, offset } = req.query;
+
+  const params = {
+    search,
+    limit,
+    offset,
+    sort,
+    filters
+  };
+
+  console.log(params);
+
+  return Promise.all([
+    getFilterOptions(Profile, params),
+    count(Profile, params),
+    searchAndFilter(Profile, params)
+  ]).then(([filters, total, profiles]) => ({ filters, total, profiles }));
+};
+
+router.get('/', (req, res, next) => {
+  Promise.resolve()
+    .then(() => getAllProfiles(req))
+    .then(foo => {
+      console.log(foo.profiles);
+      return foo;
+    })
+    .then(({ filters, total, profiles }) => {
+      res.meta.filters = filters;
+      res.meta.total = total;
+      res.meta.count = profiles.total;
+      res.response = profiles.results;
+      next();
+    })
+    .catch(next);
+});
+
+module.exports = router;
