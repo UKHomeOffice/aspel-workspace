0ff1de9756d0cbd4e0fdc1c2c7102e4f7659d492
Phil Moorhouse <phil@wheelsandcogs.co.uk>
2019-01-11 16:46:49 +0000

    display submit button only if all checkboxes are checked

--- packages/asl-components/src/application-confirm/index.jsx
+++ packages/asl-components/src/application-confirm/index.jsx
@@ -1,28 +1,51 @@
-import React from 'react';
-import { Form, Snippet } from '../';
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { CheckboxGroup } from '@ukhomeoffice/react-components';
+import { Snippet } from '../';
 
-const formatters = {
-  declaration: {
-    mapOptions: option => {
-      return {
-        ...option,
-        reveal: (
-          <button type="submit" className="govuk-button"><Snippet>actions.submit</Snippet></button>
-        )
-      };
-    }
+class ApplicationConfirm extends Component {
+  componentDidMount() {
+    this.setState({
+      declarationCount: this.props.declarations.length,
+      checkedCount: 0,
+      disabled: true
+    });
   }
-};
 
-export default () => (
-  <Form
-    formatters={formatters}
-    className="application-confirm"
-    model={{
-      declaration: []
-    }}
-    submit={false}
-  >
-    <input type="hidden" name="action" value="submit-pil-application" />
-  </Form>
-);
+  onChange(event) {
+    console.log(this.state);
+    console.log(event.target);
+
+    this.setState(state => {
+      const checkedCount = event.target.checked ? state.checkedCount + 1 : state.checkedCount - 1;
+
+      return {
+        checkedCount,
+        disabled: checkedCount < state.declarationCount
+      };
+    });
+
+    return true;
+  }
+
+  render() {
+    return (
+      <div className="application-confirm">
+        <CheckboxGroup
+          name="declarations"
+          label={<Snippet>declaration.title</Snippet>}
+          options={this.props.declarations}
+          error={this.props.error}
+          onChange={this.onChange.bind(this)}
+        />
+        <button type="submit" className="govuk-button" disabled={!this.state || this.state.disabled}>
+          <Snippet>buttons.submit</Snippet>
+        </button>
+      </div>
+    );
+  }
+}
+
+const mapStateToProps = ({ static: { declarations, error } }) => ({ declarations, error });
+
+export default connect(mapStateToProps)(ApplicationConfirm);
