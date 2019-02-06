63b16aa0dfd3b7b5b6ad453facfa1b23d876dc6b
Phil Moorhouse <phil@wheelsandcogs.co.uk>
2019-02-06 14:35:54 +0000

    allow user to remove species selectors (except first)

--- packages/asl-components/src/add-another/index.jsx
+++ packages/asl-components/src/add-another/index.jsx
@@ -20,25 +20,42 @@ class AddAnother extends Component {
     this.setState({ visible });
   }
 
+  remove(e, index) {
+    e.preventDefault();
+    const visible = [ ...this.state.visible ];
+    visible[index] = false;
+    this.setState({ visible });
+  }
+
   render() {
     const {
       children,
-      label,
+      labelAdd,
+      labelRemove,
       totalCount
     } = this.props;
     const visible = (this.state && this.state.visible) || Array(totalCount).fill(false);
+
     return (
-      <Fragment>
-        {
-          visible
-            .filter(Boolean)
-            .map((val, i) => {
-              const id = children.props.id ? `${children.props.id}-${i}` : undefined;
-              return React.cloneElement(children, { id, key: i });
-            })
-        }
-        <div><a href="#" onClick={e => this.addAnother(e)}>{label}</a></div>
-      </Fragment>
+      <div className="add-another">
+        { visible.filter(Boolean).map((isVisible, index) => {
+          const id = children.props.id ? `${children.props.id}-${index}` : undefined;
+          return isVisible &&
+            <div key={index} className="govuk-grid-row add-another-item">
+              <div className="govuk-grid-column-three-quarters">
+                { React.cloneElement(children, { id, key: index }) }
+              </div>
+              <div className="govuk-grid-column-one-quarter">
+                { index > 0 &&
+                  <a href="#" className="add-another-remove" onClick={e => this.remove(e, index)}>{labelRemove || 'Remove'}</a>
+                }
+              </div>
+            </div>;
+        })}
+        <div>
+          <a href="#" className="add-another-add" onClick={e => this.addAnother(e)}>{labelAdd || 'Add'}</a>
+        </div>
+      </div>
     );
   }
 }
