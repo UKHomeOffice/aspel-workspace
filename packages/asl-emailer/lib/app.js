8fb700b923289613b90dd44ce6e2bbf6a5f1aad6
Elvin Ali <ali.elvin@gmail.com>
2019-03-12 09:50:27 +0000

    *WIP* Add notification templates

--- packages/asl-emailer/lib/app.js
+++ packages/asl-emailer/lib/app.js
@@ -3,9 +3,11 @@ const path = require('path');
 const api = require('@asl/service/api');
 const Mailer = require('snailmail');
 
+const bodyParser = require('body-parser');
+
 module.exports = settings => {
   const app = api(settings);
-
+  app.use(bodyParser.json());
   const mailer = Mailer({
     ...settings.email,
     templateDir: path.resolve(__dirname, '../templates'),
@@ -18,10 +20,18 @@ module.exports = settings => {
   app.post('/:template', (req, res) => {
     const params = {
       template: req.params.template,
-      to: req.body.to,
+      // to: req.body.to,
+      to: 'swagbag@swagbag.club',
       subject: req.body.subject,
       data: req.body
     };
+
+    // res.json(req.params.template, {...req.body});
+
+    console.log('-----------------REQUEST BODY BEGIN---------------------------');
+    console.log(JSON.stringify(req.body));
+    console.log('-----------------REQUEST BODY END---------------------------');
+
     mailer.send(params)
       .then(() => {
         res.json({});
