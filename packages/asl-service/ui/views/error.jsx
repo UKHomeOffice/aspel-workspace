486bac31e3a6685d72f9e2d93ba8ea38b3d3d062
Leonard Martin <leonard.martin@gmail.com>
2019-04-02 09:39:01 +0100

    Add 403 specific error page
    When signing up ASRU users for the first time they will be greeted with this error. Make it a bit nicer.

--- packages/asl-service/ui/views/error.jsx
+++ packages/asl-service/ui/views/error.jsx
@@ -1,8 +1,18 @@
 import React, { Fragment } from 'react';
 
-export default ({ error }) => <Fragment>
-  <h1 className="heading-xlarge">{ error.message }</h1>
-  {
-    error.status > 499 && error.stack && <pre>{ error.stack }</pre>
+export default ({ error }) => {
+  switch (error.status) {
+    case 403:
+      return <Fragment>
+        <h1 className="heading-xlarge">You do not have permission to access this page</h1>
+        <p>Please contact an administrator to request access</p>
+      </Fragment>;
+    default:
+      return <Fragment>
+        <h1 className="heading-xlarge">{ error.message }</h1>
+        {
+          error.status > 499 && error.stack && <pre>{ error.stack }</pre>
+        }
+      </Fragment>;
   }
-</Fragment>;
+};
