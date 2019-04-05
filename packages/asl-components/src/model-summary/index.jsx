d4a529a475162a1caa34f8fc462d5ce9f96bf9e9
Phil Moorhouse <phil@wheelsandcogs.co.uk>
2019-04-05 14:17:26 +0100

    use the schema to order the rendered fields if present

--- packages/asl-components/src/model-summary/index.jsx
+++ packages/asl-components/src/model-summary/index.jsx
@@ -1,7 +1,6 @@
 import classnames from 'classnames';
 import React, { Fragment } from 'react';
 import map from 'lodash/map';
-import pickBy from 'lodash/pickBy';
 import size from 'lodash/size';
 import { connect } from 'react-redux';
 import { Snippet } from '../';
@@ -16,7 +15,12 @@ const getValue = (value, format, model) => {
 const ModelSummary = ({ model, schema, formatters = {}, className }) => {
   let fields = model;
   if (size(schema)) {
-    fields = pickBy(model, (field, key) => schema[key] && schema[key].show !== false);
+    fields = {};
+    Object.keys(schema).map(key => {
+      if (schema[key].show !== false) {
+        fields[key] = model[key];
+      }
+    });
   }
   return (
     <dl className={classnames('model-summary', 'inline', className)}>
