b18ffd4fdf4624dfe29df638461f3932e947e0e3
Phil Moorhouse <phil@wheelsandcogs.co.uk>
2019-07-23 14:22:50 +0100

    smooth scroll

--- packages/asl-components/src/back-to-top/index.jsx
+++ packages/asl-components/src/back-to-top/index.jsx
@@ -30,7 +30,11 @@ class BackToTop extends Component {
   }
 
   scrollToTop() {
-    window.scrollTo(0, 0);
+    window.scrollTo({
+      top: 0,
+      left: 0,
+      behavior: 'smooth'
+    });
   }
 
   render() {
