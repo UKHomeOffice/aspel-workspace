83cdddb8675286de718ec0a10cd99e4e356fad1a
Phil Moorhouse <phil@wheelsandcogs.co.uk>
2019-08-01 16:53:55 +0100

    add some tests

--- packages/asl-components/src/expiry-date/index.spec.jsx
+++ packages/asl-components/src/expiry-date/index.spec.jsx
@@ -2,6 +2,9 @@ import React from 'react';
 import { shallow } from 'enzyme';
 import ExpiryDate from './';
 import Snippet from '../snippet';
+import endOfTomorrow from 'date-fns/end_of_tomorrow';
+import addWeeks from 'date-fns/add_weeks';
+import addMonths from 'date-fns/add_months';
 
 describe('<ExpiryDate />', () => {
 
@@ -9,6 +12,29 @@ describe('<ExpiryDate />', () => {
     const wrapper = shallow(<ExpiryDate date={'2017-01-01'} />);
     expect(wrapper.find(Snippet).length).toBe(1);
     expect(wrapper.find(Snippet).props().children).toEqual('diff.expired');
+  });
+
+  test('shows urgent 1 day left message if the expiry date is tomorrow', () => {
+    const wrapper = shallow(<ExpiryDate date={endOfTomorrow()} />);
+    expect(wrapper.find(Snippet).length).toBe(1);
+    expect(wrapper.find(Snippet).props().children).toEqual('diff.singular');
+    expect(wrapper.find('span').props().className).toMatch('urgent');
+    expect(wrapper.find(Snippet).props().unit).toEqual('day');
+    expect(wrapper.find(Snippet).props().diff).toEqual(1);
+  });
+
+  test('shows less than x weeks left if the expiry date is greater than 1 week but less than a month', () => {
+    const wrapper = shallow(<ExpiryDate date={addWeeks(new Date(), 3)} />);
+    expect(wrapper.find(Snippet).length).toBe(1);
+    expect(wrapper.find(Snippet).props().unit).toEqual('week');
+    expect(wrapper.find(Snippet).props().diff).toEqual(3);
+  });
+
+  test('shows less than x months left if the expiry date is greater than 1 month but less than a year', () => {
+    const wrapper = shallow(<ExpiryDate date={addMonths(new Date(), 9)} />);
+    expect(wrapper.find(Snippet).length).toBe(1);
+    expect(wrapper.find(Snippet).props().unit).toEqual('month');
+    expect(wrapper.find(Snippet).props().diff).toEqual(9);
   });
 
 });
