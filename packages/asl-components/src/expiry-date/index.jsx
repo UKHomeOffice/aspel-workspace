479e46c48c495bf8666e66c720730e7685cf778e
Phil Moorhouse <phil@wheelsandcogs.co.uk>
2019-08-01 16:14:19 +0100

    refactor to allow different units on notices

--- packages/asl-components/src/expiry-date/index.jsx
+++ packages/asl-components/src/expiry-date/index.jsx
@@ -1,52 +1,43 @@
 import React, { Fragment } from 'react';
 import { Snippet } from '../';
-import differenceInMonths from 'date-fns/difference_in_calendar_months';
-import differenceInWeeks from 'date-fns/difference_in_calendar_weeks';
+import differenceInMonths from 'date-fns/difference_in_months';
+import differenceInWeeks from 'date-fns/difference_in_weeks';
 import differenceInDays from 'date-fns/difference_in_calendar_days';
+import isBefore from 'date-fns/is_before';
 import format from 'date-fns/format';
 import classnames from 'classnames';
 
-const ExpiryDate = ({
-  date,
-  dateFormat,
-  unit = 'month',
-  adjustment = 0,
-  showUrgent = 3,
-  showNotice = 11
-}) => {
+const ExpiryDate = ({ date, dateFormat, unit, adjustment, showUrgent, showNotice }) => {
   if (!date) {
     return null;
   }
 
-  let differenceFunction;
-  switch (unit) {
-    case 'month':
-      differenceFunction = differenceInMonths;
-      break;
-    case 'week':
-      differenceFunction = differenceInWeeks;
-      break;
-    case 'day':
-      differenceFunction = differenceInDays;
-      break;
+  const now = new Date();
+
+  const diff = {
+    day: differenceInDays(date, now) + adjustment,
+    week: differenceInWeeks(date, now) + adjustment,
+    month: differenceInMonths(date, now) + adjustment
   }
-  const diff = differenceFunction(date, new Date()) + adjustment;
-  const urgent = diff < showUrgent;
+
+  const displayUnit = diff['month'] > 0 ? 'month' : (diff['week'] > 0 ? 'week' : 'day');
+  const displayDiff = diff['month'] > 0 ? diff['month'] : (diff['week'] > 0 ? diff['week'] : diff['day']);
+  const urgent = diff[unit] <= showUrgent;
 
   let contentKey = 'diff.standard';
 
-  if (diff <= 0) {
+  if (isBefore(date, new Date())) {
     contentKey = 'diff.expired';
   } else if (urgent) {
-    contentKey = diff === 1 ? 'diff.singular' : 'diff.plural';
+    contentKey = displayDiff === 1 ? 'diff.singular' : 'diff.plural';
   }
 
   return (
     <Fragment>
       {format(date, dateFormat)}
-      {diff <= showNotice && (
+      {diff[unit] <= showNotice && (
         <span className={classnames('notice', { urgent })}>
-          <Snippet diff={diff} unit={unit}>{contentKey}</Snippet>
+          <Snippet diff={displayDiff} unit={displayUnit}>{contentKey}</Snippet>
         </span>
       )}
     </Fragment>
@@ -54,7 +45,11 @@ const ExpiryDate = ({
 };
 
 ExpiryDate.defaultProps = {
-  dateFormat: 'DD MMMM YYYY'
+  dateFormat: 'DD MMMM YYYY',
+  unit: 'month',
+  adjustment: 0,
+  showUrgent: 3,
+  showNotice: 11
 };
 
 export default ExpiryDate;
