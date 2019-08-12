c6bf4c7739602570b258ccb46f37c4e2700317bd
Phil Moorhouse <phil@wheelsandcogs.co.uk>
2019-08-12 17:15:54 +0100

    fix the tests

---
+++ packages/asl-components/src/countdown/index.spec.jsx
@@ -0,0 +1,39 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import Countdown from './';
+import Snippet from '../snippet';
+import endOfTomorrow from 'date-fns/end_of_tomorrow';
+import addWeeks from 'date-fns/add_weeks';
+import addMonths from 'date-fns/add_months';
+
+describe('<Countdown />', () => {
+
+  test('shows an expired message if provided date is in the past', () => {
+    const wrapper = shallow(<Countdown expiry={'2017-01-01'} />);
+    expect(wrapper.find(Snippet).props().children).toEqual('diff.expired');
+  });
+
+  test('shows urgent 1 day left message if the expiry date is tomorrow', () => {
+    const wrapper = shallow(<Countdown expiry={endOfTomorrow()} />);
+    expect(wrapper.find(Snippet).length).toBe(1);
+    expect(wrapper.find(Snippet).props().children).toEqual('diff.singular');
+    expect(wrapper.find('span').props().className).toMatch('urgent');
+    expect(wrapper.find(Snippet).props().unit).toEqual('day');
+    expect(wrapper.find(Snippet).props().diff).toEqual(1);
+  });
+
+  test('shows less than x weeks left if the expiry date is greater than 1 week but less than a month', () => {
+    const wrapper = shallow(<Countdown expiry={addWeeks(new Date(), 2)} />);
+    expect(wrapper.find(Snippet).length).toBe(1);
+    expect(wrapper.find(Snippet).props().unit).toEqual('week');
+    expect(wrapper.find(Snippet).props().children).toEqual('diff.plural');
+  });
+
+  test('shows less than x months left if the expiry date is greater than 1 month but less than a year', () => {
+    const wrapper = shallow(<Countdown expiry={addMonths(new Date(), 9)} />);
+    expect(wrapper.find(Snippet).length).toBe(1);
+    expect(wrapper.find(Snippet).props().unit).toEqual('month');
+    expect(wrapper.find(Snippet).props().children).toEqual('diff.plural');
+  });
+
+});
--- packages/asl-components/src/expiry-date/index.spec.jsx
+++ packages/asl-components/src/expiry-date/index.spec.jsx
@@ -1,40 +1,13 @@
 import React from 'react';
 import { shallow } from 'enzyme';
 import ExpiryDate from './';
-import Snippet from '../snippet';
-import endOfTomorrow from 'date-fns/end_of_tomorrow';
-import addWeeks from 'date-fns/add_weeks';
-import addMonths from 'date-fns/add_months';
+import Countdown from '../countdown';
 
 describe('<ExpiryDate />', () => {
 
-  test('shows an expired message if provided date is in the past', () => {
+  test('shows a message if provided date is in the past', () => {
     const wrapper = shallow(<ExpiryDate date={'2017-01-01'} />);
-    expect(wrapper.find(Snippet).length).toBe(1);
-    expect(wrapper.find(Snippet).props().children).toEqual('diff.expired');
-  });
-
-  test('shows urgent 1 day left message if the expiry date is tomorrow', () => {
-    const wrapper = shallow(<ExpiryDate date={endOfTomorrow()} />);
-    expect(wrapper.find(Snippet).length).toBe(1);
-    expect(wrapper.find(Snippet).props().children).toEqual('diff.singular');
-    expect(wrapper.find('span').props().className).toMatch('urgent');
-    expect(wrapper.find(Snippet).props().unit).toEqual('day');
-    expect(wrapper.find(Snippet).props().diff).toEqual(1);
-  });
-
-  test('shows less than x weeks left if the expiry date is greater than 1 week but less than a month', () => {
-    const wrapper = shallow(<ExpiryDate date={addWeeks(new Date(), 2)} />);
-    expect(wrapper.find(Snippet).length).toBe(1);
-    expect(wrapper.find(Snippet).props().unit).toEqual('week');
-    expect(wrapper.find(Snippet).props().children).toEqual('diff.plural');
-  });
-
-  test('shows less than x months left if the expiry date is greater than 1 month but less than a year', () => {
-    const wrapper = shallow(<ExpiryDate date={addMonths(new Date(), 9)} />);
-    expect(wrapper.find(Snippet).length).toBe(1);
-    expect(wrapper.find(Snippet).props().unit).toEqual('month');
-    expect(wrapper.find(Snippet).props().children).toEqual('diff.standard');
+    expect(wrapper.find(Countdown).length).toBe(1);
   });
 
 });
