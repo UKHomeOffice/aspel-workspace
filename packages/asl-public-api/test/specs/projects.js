029178bf4369d4fd48ba2b93eef2f9d79f8972ef
Phil Moorhouse <phil@wheelsandcogs.co.uk>
2019-08-29 15:32:15 +0100

    fix the tests

--- packages/asl-public-api/test/specs/projects.js
+++ packages/asl-public-api/test/specs/projects.js
@@ -4,6 +4,7 @@ const apiHelper = require('../helpers/api');
 
 const INACTIVE_PROJECT_ID = 'bf22f7cd-cf85-42ef-93da-02b709df67be';
 const ACTIVE_PROJECT_ID = 'd2f9777d-2d9d-4ea2-a9c2-c5ed592fd98d';
+const LICENCE_HOLDER_ID = 'f0835b01-00a0-4c7f-954c-13ed2ef7efd9';
 
 describe('/projects', () => {
   before(() => {
@@ -51,10 +52,11 @@ describe('/projects', () => {
           assert.equal(this.workflow.handler.callCount, 1);
           const req = this.workflow.handler.firstCall.args[0];
           const body = req.body;
-          assert.equal(req.method, 'PUT');
+          assert.equal(req.method, 'POST');
           assert.equal(body.model, 'project');
           assert.equal(body.action, 'revoke');
-          assert.deepEqual(body.data, { establishmentId: '100', id: ACTIVE_PROJECT_ID });
+          assert.equal(body.id, ACTIVE_PROJECT_ID);
+          assert.deepEqual(body.data, { establishmentId: 100, licenceHolderId: LICENCE_HOLDER_ID });
         });
     });
   });
