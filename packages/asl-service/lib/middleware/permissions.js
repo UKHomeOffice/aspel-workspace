b555e97d07426c1d9a8eda54d1a8dbb839ded547
Joe Fitter <joefitter@hotmail.com>
2019-12-12 13:29:38 +0000

    Don't overwrite provided establishment
    * Establishment is sometimes relevant to the model and can be different from the one held in req.params. In particular viewing a PIL at a different establishment

--- packages/asl-service/lib/middleware/permissions.js
+++ packages/asl-service/lib/middleware/permissions.js
@@ -2,7 +2,7 @@ const { UnauthorisedError } = require('../../errors');
 
 module.exports = (task, params = {}) => {
   return (req, res, next) => {
-    req.user.can(task, Object.assign({}, req.params, params, { establishment: req.establishmentId }))
+    req.user.can(task, Object.assign({}, req.params, { establishment: req.establishmentId }, params))
       .then(allowed => {
         return allowed ? next() : next(new UnauthorisedError());
       });
