df86437e2878242a300e05dc2847b7109e7b2012
Leonard Martin <leonard.martin@gmail.com>
2020-01-09 18:09:51 +0000

    Use schema methods for simplifying billable queries

--- packages/asl-public-api/lib/routers/establishment/billing.js
+++ packages/asl-public-api/lib/routers/establishment/billing.js
@@ -1,6 +1,7 @@
 const { pick } = require('lodash');
-const { permissions } = require('../../middleware');
 const { Router } = require('express');
+const { permissions } = require('../../middleware');
+const { NotFoundError } = require('@asl/service/errors');
 
 const router = Router({ mergeParams: true });
 
@@ -21,43 +22,101 @@ const populateDates = (id, start, end) => pil => {
 };
 
 const cleanSensitiveData = (id) => pil => {
-  pil.profile.establishments = pil.profile.establishments.filter(e => e.id === id);
   pil.profile = pick(pil.profile, 'id', 'firstName', 'lastName', 'establishments');
   pil.establishmentId = id;
   return pick(pil, 'id', 'establishmentId', 'licenceNumber', 'profile', 'startDate', 'endDate');
 };
 
-router.get('/',
-  permissions('establishment.licenceFees'),
-  (req, res, next) => {
-    const { limit, offset, sort = {} } = req.query;
-    const { PIL } = req.models;
-
-    const year = parseInt(req.query.year, 10);
-    const start = `${year}-04-06`;
-    const end = `${year + 1}-04-05`;
-
-    const params = {
-      establishmentId: req.establishment.id,
-      start,
-      end
-    };
-    let query = PIL.query().billable(params);
-    query = PIL.orderBy({ query, sort: { ...sort, column: sort.column || 'profile.lastName' } });
-    Promise.resolve()
-      .then(() => PIL.paginate({ query, limit, offset }))
-      .then(pils => {
-        res.meta.count = pils.total;
-        res.meta.total = pils.total;
-        res.meta.startDate = start;
-        res.meta.endDate = end;
-        res.response = pils.results
-          .map(populateDates(req.establishment.id, start, end))
-          .map(cleanSensitiveData(req.establishment.id));
-      })
-      .then(() => next('router'))
-      .catch(next);
+const fees = {
+  2017: {
+    pel: 500,
+    pil: 250
+  },
+  2018: {
+    pel: 550,
+    pil: 275
+  },
+  2019: {
+    pel: 600,
+    pil: 300
   }
-);
+};
+
+router.use(permissions('establishment.licenceFees'));
+
+router.use((req, res, next) => {
+  let year = req.query.year;
+  if (!year) {
+    year = Object.keys(fees).pop();
+  }
+  if (!fees[year]) {
+    return next(new NotFoundError());
+  }
+
+  year = parseInt(year, 10);
+
+  const start = `${year}-04-06`;
+  const end = `${year + 1}-04-05`;
+  req.fees = fees[year];
+  res.meta.startDate = start;
+  res.meta.endDate = end;
+  res.meta.year = year;
+  res.meta.years = Object.keys(fees).map(y => parseInt(y, 10));
+  next();
+});
+
+router.get('/', (req, res, next) => {
+  const { PIL } = req.models;
+
+  const params = {
+    establishmentId: req.establishment.id,
+    start: res.meta.startDate,
+    end: res.meta.endDate
+  };
+  Promise.resolve()
+    .then(() => PIL.query().whereBillable(params).count())
+    .then(result => {
+      const count = parseInt(result[0].count, 10);
+      res.response = {};
+      res.response.fees = req.fees;
+      res.response.numberOfPils = count;
+      res.response.pils = req.fees.pil * count;
+      res.response.pel = req.fees.pel;
+      res.response.total = res.response.pils + res.response.pel;
+    })
+    .then(() => next())
+    .catch(next);
+});
+
+router.get('/pils', (req, res, next) => {
+  const { limit, offset, sort = {} } = req.query;
+  sort.column = sort.column || 'profile.lastName';
+  const { PIL } = req.models;
+  const start = res.meta.startDate;
+  const end = res.meta.endDate;
+
+  const params = {
+    establishmentId: req.establishment.id,
+    start,
+    end
+  };
+
+  Promise.resolve()
+    .then(() => {
+      let query = PIL.query().billable(params);
+      query = PIL.orderBy({ query, sort });
+      query = PIL.paginate({ query, limit, offset });
+      return query;
+    })
+    .then(pils => {
+      res.meta.count = pils.total;
+      res.meta.total = pils.total;
+      res.response = pils.results
+        .map(populateDates(req.establishment.id, start, end))
+        .map(cleanSensitiveData(req.establishment.id));
+    })
+    .then(() => next())
+    .catch(next);
+});
 
 module.exports = router;
