40d5e3746f9fa393da05ce6b4cef0088e23bb338
Phil Moorhouse <phil@wheelsandcogs.co.uk>
2020-01-22 15:20:51 +0000

    add failing test to show handling of false is required

--- packages/asl-components/src/countdown/index.jsx
+++ packages/asl-components/src/countdown/index.jsx
@@ -8,9 +8,9 @@ import isToday from 'date-fns/is_today';
 import classnames from 'classnames';
 
 const Countdown = ({ expiry, unit, showNotice, showUrgent }) => {
-  if (showNotice === false) {
-    return null;
-  }
+  // if (showNotice === false) {
+  //   return null;
+  // }
 
   const now = new Date();
 
