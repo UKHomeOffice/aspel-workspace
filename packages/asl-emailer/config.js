42547c7a0ec6d6e070ebf2061fc2802d627dd04e
Leonard Martin <leonard.martin@gmail.com>
2020-04-24 15:13:06 +0100

    Move email sending to GOVUK Notify
    * convert html templates to plain text
    * include template rendering code that was previously handled by snailmail module
    * use notify client to send emails

--- packages/asl-emailer/config.js
+++ packages/asl-emailer/config.js
@@ -2,10 +2,7 @@ module.exports = {
   port: process.env.PORT || 8080,
   whitelist: (process.env.DOMAIN_WHITELIST || '').split(',').filter(Boolean),
   email: {
-    from: process.env.EMAIL_FROM_ADDRESS,
-    replyTo: process.env.EMAIL_REPLY_TO,
-    key: process.env.EMAIL_ACCESS_KEY,
-    secret: process.env.EMAIL_SECRET,
-    region: process.env.EMAIL_REGION
+    apiKey: process.env.EMAILER_API_KEY,
+    template: process.env.EMAILER_TEMPLATE_KEY
   }
 };
