f5316f4aab7ed2ded0324e3b17b2487cb6cd0d7b
Leonard Martin <leonard.martin@gmail.com>
2020-06-29 11:52:09 +0100

    Add tasks report
    Gets all the tasks resolved after a defined date and returns task type, date closed and the number of iterations between establishment and ASRU.

---
+++ packages/asl-metrics/lib/reports/tasks/index.js
@@ -0,0 +1,36 @@
+const { get } = require('lodash');
+
+module.exports = ({ db, query: params, flow }) => {
+
+  const query = () => {
+    return db.flow('cases')
+      .leftJoin('activity_log', 'cases.id', 'activity_log.case_id')
+      .select([
+        'cases.*',
+        db.flow.raw('JSON_AGG(activity_log.event_name) as activity')
+      ])
+      .where({ status: 'resolved' })
+      .whereRaw(`data->>'establishmentId' != '1502162'`)
+      .where('cases.updated_at', '>', params.since || '2019-07-01')
+      .groupBy('cases.id');
+  };
+
+  const parse = record => {
+    const model = record.data.model;
+    const schemaVersion = get(record, 'data.modelData.schemaVersion', 0);
+    let action = record.data.action;
+
+    if (['pil', 'project'].includes(model) && action === 'grant') {
+      const isAmendment = get(record, 'data.modelData.status') === 'active';
+      action = isAmendment ? 'amendment' : 'application';
+    }
+
+    const iterations = record.activity.filter(e => e.match(/^status:[.]*:resubmitted/)).length + 1;
+    const { updatedAt } = record;
+
+    return { model, action, schemaVersion, iterations, updatedAt };
+  };
+
+  return { query, parse };
+
+};
