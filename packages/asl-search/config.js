2148aa3253cac030958f1b654d197418d4baecfe
Phil Moorhouse <phil@wheelsandcogs.co.uk>
2020-07-01 15:23:18 +0100

    use london region
    Co-authored-by: Leonard Martin <leonard.martin@gmail.com>

--- packages/asl-search/config.js
+++ packages/asl-search/config.js
@@ -4,7 +4,7 @@ module.exports = {
 
   es: {
     aws: {
-      region: process.env.AWS_REGION || 'eu-west-1',
+      region: process.env.AWS_REGION || 'eu-west-2',
       credentials: {
         key: process.env.AWS_ES_KEY,
         secret: process.env.AWS_ES_SECRET
