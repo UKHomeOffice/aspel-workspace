3f5c9983319b8ef77df01d71b3b8748b0a8d70ae
Joe Fitter <joefitter@hotmail.com>
2020-08-14 14:53:48 +0100

    Feature - training courses
    * Updated autocomplete to get data from an endpoint
    * Updated model summary to use accessor

--- packages/asl-components/src/autocomplete/index.jsx
+++ packages/asl-components/src/autocomplete/index.jsx
@@ -1,10 +1,13 @@
 import React, { useState, useEffect } from 'react';
 import AccessibleAutocomplete from 'accessible-autocomplete/react';
 import { InputWrapper } from '@ukhomeoffice/react-components';
+import { getUrl } from '../link';
+import fetch from 'r2';
 
 export default function AutoComplete(props) {
   const [value, setValue] = useState(props.value);
-  const defaultValue = (props.options.find(opt => opt.value === props.value) || {}).label;
+  const defaultValue = props.defaultValue || (props.options.find(opt => opt.value === props.value) || {}).label;
+  const apiPath = props.apiPath ? getUrl({ page: props.apiPath }) : null;
 
   useEffect(() => {
     const inputElement = window.document.querySelector(`input#${props.name}`);
@@ -23,11 +26,22 @@ export default function AutoComplete(props) {
     };
   });
 
-  function suggest (query, syncResults) {
+  function simpleSearch (query, syncResults) {
     syncResults(query
       ? props.options.filter(result => result.label.toLowerCase().includes(query.toLowerCase()))
       : []
     );
+  }
+
+  function apiSearch (query, syncResults) {
+    if (!query) {
+      return syncResults([]);
+    }
+    fetch(`${apiPath}/get-autocomplete-options/${query}`).json
+      .then(results => {
+        syncResults(results || []);
+      })
+      .catch(err => console.error('Failed to fetch', err));
   }
 
   function renderLabel(item) {
@@ -39,7 +53,7 @@ export default function AutoComplete(props) {
       <input type="hidden" name={props.name} value={value} />
       <AccessibleAutocomplete
         id={props.name}
-        source={suggest}
+        source={props.apiPath ? apiSearch : simpleSearch}
         templates={{
           inputValue: renderLabel,
           suggestion: renderLabel
