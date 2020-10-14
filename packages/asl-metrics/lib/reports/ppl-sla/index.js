2e4eebb7843f32c133df8218d475e64a7f683185
Leonard Martin <leonard.martin@gmail.com>
2020-10-14 16:05:48 +0100

    Use data fields directly for PPL SLA report
    Instead of reverse engineering the activity log just use the data fields that are there.

--- packages/asl-metrics/lib/reports/ppl-sla/index.js
+++ packages/asl-metrics/lib/reports/ppl-sla/index.js
@@ -1,148 +1,52 @@
 const { bankHolidays } = require('@asl/constants');
-const moment = require('moment-business-time');
 const { get } = require('lodash');
+const moment = require('moment-business-time');
 
 // configure bank holidays
 moment.updateLocale('en', { holidays: bankHolidays });
 
 module.exports = ({ db, query: params, flow }) => {
 
-  const since = (params && params.since) ? moment(params.since, 'YYYY-MM-DD') : null;
+  const since = (params && params.since) ? moment(params.since, 'YYYY-MM-DD').format('YYYY-MM-DD') : null;
 
   const query = () => {
     return db.flow('cases')
-      .leftJoin('activity_log', 'cases.id', 'activity_log.case_id')
-      .select([
-        'cases.*',
-        db.flow.raw('JSON_AGG(activity_log.* ORDER BY activity_log.created_at asc) as activity')
-      ])
+      .whereRaw(`(cases.data->>'deadlinePassed')::boolean = true`)
       .whereRaw(`cases.data->>'model' = 'project'`)
       .whereRaw(`cases.data->>'action' = 'grant'`)
       .whereRaw(`cases.data->'modelData'->>'status' = 'inactive'`)
-      .where('cases.created_at', '<', moment().subtract(40, 'days').format('YYYY-MM-DD'))
       .where(function () {
-        // ignore comment-related activity
-        this
-          .where('activity_log.event_name', 'like', 'status:%')
-          .orWhere('activity_log.event_name', '=', 'update');
-      })
-      .where(function () {
-        // to have passed a deadline after the cutoff then the last activity must have been less than 55 days ago
-        // can safely disregard tasks untouched for more than 55 working days
-        since && this.where('cases.updated_at', '>', since.subtract(55, 'days').format('YYYY-MM-DD'));
-      })
-      .groupBy('cases.id');
+        since && this
+          .whereRaw(`cases.data->>'deadlinePassedDate' is not null`)
+          .whereRaw(`(cases.data->>'deadlinePassedDate')::date > '${since}'`);
+      });
   };
 
   const parse = record => {
-
-    const getDeadlineState = () => {
-      const currentState = record.activity.reduce((state, activity) => {
-
-        // once deadline has passed we don't need to do any more processing
-        if (state.hasPassed) {
-          return state;
-        }
-
-        // check if a deadline has passed since last activity
-        if (state.withASRU && state.isCompleteAndCorrect) {
-          const deadline = moment(state.submitted).addWorkingTime(state.extended ? 55 : 40, 'days');
-          const hasPassed = deadline.isBefore(activity.created_at, 'day');
-          if (hasPassed) {
-            return {
-              ...state,
-              deadline,
-              hasPassed
-            };
-          }
-        }
-
-        // if activity was not a status change then the only thing we're interested in is
-        // if it touched the `extended` property
-        if (activity.event_name === 'update') {
-          return {
-            ...state,
-            extended: get(activity, 'event.data.extended', false)
-          };
-        }
-
-        const status = flow[activity.event.status];
-        const isSubmission = !state.withASRU && status.withASRU;
-        const isClosedOrReturned = !status.open || (state.withASRU && !status.withASRU);
-
-        const meta = get(activity, 'event.data.meta', {});
-        const isCompleteAndCorrect = ['authority', 'awerb', 'ready'].every(declaration => {
-          return meta[declaration] && meta[declaration].toLowerCase() === 'yes';
-        });
-
-        // if it's a submission to the inspector then make note of the date and mark record as with ASRU
-        if (isSubmission) {
-          return {
-            ...state,
-            withASRU: true,
-            isCompleteAndCorrect,
-            submitted: activity.created_at
-          };
-        }
-
-        // if the record is being returned or closed then flag as not being with ASRU
-        if (isClosedOrReturned) {
-          return {
-            ...state,
-            withASRU: false
-          };
-        }
-
-        return state;
-      }, { withASRU: false });
-
-      // if the last activity left the project in an open submitted state then check if the deadline has passed
-      if (!currentState.hasPassed && currentState.withASRU && currentState.isCompleteAndCorrect) {
-        const deadline = moment(currentState.submitted).addWorkingTime(currentState.extended ? 55 : 40, 'days');
-        const hasPassed = deadline.isBefore(moment(), 'day');
-        if (hasPassed) {
-          currentState.deadline = deadline;
-          currentState.hasPassed = true;
-        }
-      }
-
-      return currentState;
-    };
-
-    const state = getDeadlineState();
-
-    const isSinceCutoff = deadline => {
-      if (!since) {
-        return true;
-      }
-      return moment(deadline).isAfter(since);
-    };
-
-    if (state.hasPassed && isSinceCutoff(state.deadline)) {
-      return db.asl('projects')
-        .select(
-          'projects.title',
-          'establishments.name',
-          'profiles.first_name',
-          'profiles.last_name'
-        )
-        .leftJoin('establishments', 'projects.establishment_id', 'establishments.id')
-        .leftJoin('profiles', 'projects.licence_holder_id', 'profiles.id')
-        .where({ 'projects.id': record.data.id })
-        .first()
-        .then(project => {
-          return {
-            title: project.title,
-            establishment: project.name,
-            licence_holder: `${project.first_name} ${project.last_name}`,
-            submitted: moment(state.submitted).toISOString(),
-            deadline: state.deadline.toISOString(),
-            extended: state.extended ? 'true' : 'false',
-            task: record.id
-          };
-        });
-    }
-    return Promise.resolve([]);
+    const deadline = get(record, 'data.deadlinePassedDate');
+    const extended = !!get(record, 'data.deadline.isExtended', false);
+    return db.asl('projects')
+      .select(
+        'projects.title',
+        'establishments.name',
+        'profiles.first_name',
+        'profiles.last_name'
+      )
+      .leftJoin('establishments', 'projects.establishment_id', 'establishments.id')
+      .leftJoin('profiles', 'projects.licence_holder_id', 'profiles.id')
+      .where({ 'projects.id': record.data.id })
+      .first()
+      .then(project => {
+        return {
+          title: project.title,
+          establishment: project.name,
+          licence_holder: `${project.first_name} ${project.last_name}`,
+          submitted: moment(deadline).subtractWorkingTime(extended ? 55 : 40, 'days').format('YYYY-MM-DD'),
+          deadline,
+          extended,
+          task: record.id
+        };
+      });
 
   };
 
