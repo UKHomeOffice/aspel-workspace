1a631468b3e3aefa2cd57f99cb225db2daab34e9
Phil Moorhouse <phil@wheelsandcogs.co.uk>
2020-10-28 15:15:10 +0000

    prevent filtering from clearing search

--- packages/asl-components/src/filters/actions.js
+++ packages/asl-components/src/filters/actions.js
@@ -1,6 +1,7 @@
 const { queryStringFromState } = require('../utils');
 const { fetchItems } = require('../actions');
 const merge = require('lodash/merge');
+const get = require('lodash/get');
 
 const setFilters = filters => ({
   type: 'SET_FILTERS',
@@ -9,9 +10,16 @@ const setFilters = filters => ({
 
 const changeFilters = filters => (dispatch, getState) => {
   const state = getState();
+  const searchFilter = get(state, 'datatable.filters.active[*]');
+
+  if (searchFilter) {
+    filters['*'] = searchFilter;
+  }
+
   const query = queryStringFromState(merge({}, state, {
     datatable: { filters: { active: filters } }
   }));
+
   return fetchItems(`${state.static.url}?${query}`, dispatch)
     .then(() => dispatch(setFilters(filters)));
 };
