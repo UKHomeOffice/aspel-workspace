d7966702383f7c9d2d4422e577347e477f3f2fa7
Phil Moorhouse <phil@wheelsandcogs.co.uk>
2020-11-23 17:16:20 +0000

    make the document header sticky

--- packages/asl-components/src/document-header/index.jsx
+++ packages/asl-components/src/document-header/index.jsx
@@ -1,4 +1,6 @@
-import React, { useState, Fragment } from 'react';
+import React, { Fragment, useState, useRef, useEffect } from 'react';
+import classnames from 'classnames';
+import { BackToTop } from '../';
 
 export default function DocumentHeader({ title, subtitle, backLink, children, detailsLabel = 'downloads' }) {
   const [detailsShowing, updateDetailsShowing] = useState(false);
@@ -8,25 +10,67 @@ export default function DocumentHeader({ title, subtitle, backLink, children, de
     updateDetailsShowing(!detailsShowing);
   };
 
-  return (
-    <header className="document-header">
-      <div className="page-title">
+  const [isSticky, setSticky] = useState(false);
+  const ref = useRef(null);
+  const handleScroll = () => {
+    if (ref.current) {
+      setSticky(ref.current.getBoundingClientRect().top <= 0);
+    }
+  };
+
+  useEffect(() => {
+    window.addEventListener('scroll', handleScroll);
+
+    return () => {
+      window.removeEventListener('scroll', () => handleScroll);
+    };
+  }, []);
+
+  function Headings({ title, subtitle }) {
+    if (isSticky) {
+      return (
+        <h2>
+          {
+            title
+              ? <Fragment>{title}: {subtitle}</Fragment>
+              : subtitle
+          }
+        </h2>
+      );
+    }
+
+    return (
+      <Fragment>
         { title && <h1>{title}</h1> }
         { subtitle && <h2>{subtitle}</h2> }
-        {
-          children &&
-            <Fragment>
-              <a href="#" onClick={toggleDetails} className="toggle-details">
-                { detailsShowing ? `Hide ${detailsLabel}` : `View ${detailsLabel}` }
-              </a>
-              {
-                detailsShowing && <div className="details">{children}</div>
-              }
-            </Fragment>
-        }
-      </div>
+      </Fragment>
+    );
+  }
 
-      { backLink && <div className="back-link">{ backLink }</div> }
-    </header>
+  return (
+    <div className={classnames('sticky-wrapper', { sticky: isSticky })} ref={ref}>
+
+      <header className="document-header">
+        <div className="page-title">
+          <Headings title={title} subtitle={subtitle} />
+          {
+            !isSticky && children &&
+              <Fragment>
+                <a href="#" onClick={toggleDetails} className="toggle-details">
+                  { detailsShowing ? `Hide ${detailsLabel}` : `View ${detailsLabel}` }
+                </a>
+                {
+                  detailsShowing && <div className="details">{children}</div>
+                }
+              </Fragment>
+          }
+        </div>
+
+        { !isSticky && backLink && <div className="back-link">{ backLink }</div> }
+
+        { isSticky && <BackToTop showAt={0} /> }
+      </header>
+
+    </div>
   );
 }
