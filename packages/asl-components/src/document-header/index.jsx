48d69980b6b414abc8ddb50499d020284d5e6abd
Phil Moorhouse <phil@wheelsandcogs.co.uk>
2020-11-25 12:34:21 +0000

    fixed styling

--- packages/asl-components/src/document-header/index.jsx
+++ packages/asl-components/src/document-header/index.jsx
@@ -12,6 +12,7 @@ export default function DocumentHeader({ title, subtitle, backLink, children, de
 
   const [isSticky, setSticky] = useState(false);
   const ref = useRef(null);
+
   const handleScroll = () => {
     if (ref.current) {
       setSticky(ref.current.getBoundingClientRect().top <= 0);
