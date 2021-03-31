fe0f1ab43c68369c4a560e464f8d0493a634a8cc
Leonard Martin <leonard.martin@gmail.com>
2021-03-31 12:23:46 +0100

    First

---
+++ packages/asl-data-exports/lib/worker.js
@@ -0,0 +1,42 @@
+const { performance } = require('perf_hooks');
+const db = require('@asl/schema');
+const rops = require('./rops');
+
+module.exports = settings => {
+  const models = db(settings.db);
+
+  const exporters = {
+    rops: rops({ ...settings, models })
+  };
+
+  const { Export } = models;
+
+  return () => {
+    const start = performance.now();
+    return Export.query()
+      .where({ ready: false })
+      .then(pending => {
+        console.log(`Found ${pending.length} jobs`);
+        return pending.reduce((promise, row) => {
+          return promise
+            .then(() => {
+              if (exporters[row.type]) {
+                return exporters[row.type](row)
+                  .then(result => Export.query().findById(row.id).patch({ ready: true, meta: result }))
+                  .catch(err => {
+                    console.error(`Processing failed with error:\n${err.stack}`);
+                  });
+              }
+              console.log(`Unrecognised type: ${row.type}`);
+            });
+        }, Promise.resolve());
+      })
+      .then(() => {
+        const time = performance.now() - start;
+        console.log(`Processing took: ${time}ms`);
+      })
+      .catch(err => {
+        console.error(err.stack);
+      });
+  };
+};
