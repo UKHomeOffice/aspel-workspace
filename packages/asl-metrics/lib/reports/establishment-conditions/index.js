6f95001e154b871f672ce468967d3295601772bf
Phil Moorhouse <phil@wheelsandcogs.co.uk>
2021-05-20 15:58:45 +0100

    use less convoluted query / parsing

--- packages/asl-metrics/lib/reports/establishment-conditions/index.js
+++ packages/asl-metrics/lib/reports/establishment-conditions/index.js
@@ -1,14 +1,4 @@
-const { omit } = require('lodash');
-
-const parse = establishment => establishment.authorisations.map(a => ({
-  ...omit(establishment, 'authorisations'),
-  authorisation_type: a ? a.type : null,
-  authorisation_method: a ? a.method : null,
-  authorisation_description: a ? a.description : null
-}));
-
 module.exports = ({ db }) => {
-
   const query = () => {
     const q = db.asl('establishments')
       .select(
@@ -17,16 +7,19 @@ module.exports = ({ db }) => {
         'establishments.licence_number',
         'establishments.status',
         'establishments.conditions',
-        db.asl.raw('JSON_AGG(authorisations) as authorisations')
+        'authorisations.type AS authorisation_type',
+        'authorisations.method AS authorisation_method',
+        'authorisations.description AS authorisation_description'
       )
       .leftJoin('authorisations', 'authorisations.establishment_id', 'establishments.id')
       .whereNull('establishments.deleted')
       .whereNull('authorisations.deleted')
-      .groupBy('establishments.id')
       .orderBy('establishments.name');
 
     return q;
   };
+
+  const parse = establishment => establishment;
 
   return { query, parse };
 };
