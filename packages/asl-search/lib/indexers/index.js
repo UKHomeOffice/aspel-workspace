8100852c221efe38c37358c42d1719c09a2cbe66
Phil Moorhouse <phil@wheelsandcogs.co.uk>
2021-10-13 17:07:04 +0100

    update index route

--- packages/asl-search/lib/indexers/index.js
+++ packages/asl-search/lib/indexers/index.js
@@ -1,5 +1,6 @@
 const { Router } = require('express');
-const Schema = require('@asl/schema');
+const aslSchema = require('../db');
+const taskflowDb = require('../db/taskflow');
 const { NotFoundError } = require('@asl/service/errors');
 
 const indexers = {
@@ -7,12 +8,12 @@ const indexers = {
   projects: require('./projects'),
   'projects-content': require('./projects-content'),
   profiles: require('./profiles'),
-  places: require('./places')
+  places: require('./places'),
+  tasks: require('./tasks')
 };
 
 module.exports = (settings) => {
   const app = Router();
-  const db = Schema(settings.db);
 
   app.param('index', (req, res, next, param) => {
     if (!indexers[param]) {
@@ -24,15 +25,25 @@ module.exports = (settings) => {
   app.put('/:index/:id', (req, res, next) => {
     return Promise.resolve(settings.esClient)
       .then(client => {
-        return indexers[req.params.index](db, client, { id: req.params.id })
-          .then(() => {
-            if (req.params.index === 'establishments') {
-              return indexers['places'](db, client, { establishmentId: req.params.id });
-            }
-            if (req.params.index === 'projects') {
-              return indexers['projects-content'](db, client, { id: req.params.id });
-            }
-          });
+        switch (req.params.index) {
+          case 'establishments':
+            return Promise.all([
+              indexers.establishments(aslSchema, client, { id: req.params.id }),
+              indexers.places(aslSchema, client, { establishmentId: req.params.id })
+            ]);
+
+          case 'projects':
+            return Promise.all([
+              indexers.projects(aslSchema, client, { id: req.params.id }),
+              indexers['projects-content'](aslSchema, client, { id: req.params.id })
+            ]);
+
+          case 'tasks':
+            return indexers.tasks({ aslSchema, taskflowDb, esClient: client });
+
+          default:
+            return indexers[req.params.index](aslSchema, client, { id: req.params.id });
+        }
       })
       .then(() => {
         res.json({ message: `Re-indexed ${req.params.index}:${req.params.id}` });
