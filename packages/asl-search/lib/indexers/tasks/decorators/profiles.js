9a65284ffbb33365fcac9ce03fb435cbd56c2d56
Phil Moorhouse <phil@wheelsandcogs.co.uk>
2021-10-18 16:19:05 +0100

    add subject to hydrated profiles

--- packages/asl-search/lib/indexers/tasks/decorators/profiles.js
+++ packages/asl-search/lib/indexers/tasks/decorators/profiles.js
@@ -1,23 +1,27 @@
 const Cacheable = require('./cacheable');
 const { get, pick } = require('lodash');
 
+const cleanModel = profile => pick(profile, 'id', 'firstName', 'lastName');
+
 module.exports = aslSchema => {
   const { Profile } = aslSchema;
   const cache = Cacheable();
 
   return async task => {
+    const subjectId = get(task, 'data.subject');
     const licenceHolderId = get(task, 'data.modelData.licenceHolderId') || get(task, 'data.data.licenceHolderId');
-
-    if (licenceHolderId) {
-      const licenceHolder = await cache.query(Profile, licenceHolderId);
-      task.licenceHolder = pick(licenceHolder, 'id', 'firstName', 'lastName');
-    }
-
     const assignedAsruId = get(task, 'assignedTo');
 
+    if (subjectId) {
+      task.subject = cleanModel(await cache.query(Profile, subjectId));
+    }
+
+    if (licenceHolderId) {
+      task.licenceHolder = cleanModel(await cache.query(Profile, licenceHolderId));
+    }
+
     if (assignedAsruId) {
-      const asruUser = await cache.query(Profile, assignedAsruId);
-      task.asruUser = pick(asruUser, 'id', 'firstName', 'lastName');
+      task.assignedTo = cleanModel(await cache.query(Profile, assignedAsruId));
     }
 
     return task;
