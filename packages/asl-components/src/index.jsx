84edc56b0dba3eacb3141af245bb73ac14634a6d
Joe Fitter <joefitter@hotmail.com>
2021-12-02 16:04:10 +0100

    Feature - link filter list
    * Added link filter list component using grid to align cols

--- packages/asl-components/src/index.jsx
+++ packages/asl-components/src/index.jsx
@@ -39,6 +39,7 @@ export { default as JoinAcronyms } from './join-acronyms';
 export { default as LicenceStatusBanner } from './licence-status-banner';
 export { default as Link } from './link';
 export { default as LinkFilter } from './link-filter';
+export { default as LinkFilterList } from './link-filter-list';
 export { default as Markdown } from './markdown';
 export { default as ModelSummary } from './model-summary';
 export { default as Metric } from './metric';
