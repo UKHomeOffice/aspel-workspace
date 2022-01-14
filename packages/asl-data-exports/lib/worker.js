de0ab4ce56da8397eeff19459b8a1fd540302302
Phil Moorhouse <phil@wheelsandcogs.co.uk>
2022-01-14 17:29:54 +0000

    add task metrics export and scheduling

--- packages/asl-data-exports/lib/worker.js
+++ packages/asl-data-exports/lib/worker.js
@@ -1,22 +1,49 @@
 const { performance } = require('perf_hooks');
+const moment = require('moment');
 const db = require('@asl/schema');
 const rops = require('./rops');
+const taskMetrics = require('./task-metrics');
+const Logger = require('./utils/logger');
 
-module.exports = settings => {
+module.exports = settings => () => {
+  const logger = Logger(settings);
+  settings.logger = logger;
+
   const models = db(settings.db);
+  settings.models = models;
 
   const exporters = {
-    rops: rops({ ...settings, models })
+    rops: rops(settings),
+    'task-metrics': taskMetrics(settings)
   };
 
   const { Export } = models;
 
-  return () => {
+  const scheduleTaskMetrics = async () => {
+    const prevMonth = moment().subtract(1, 'month');
+    const start = moment(prevMonth).startOf('month').format('YYYY-MM-DD');
+    const end = moment(prevMonth).endOf('month').format('YYYY-MM-DD');
+    const type = 'task-metrics';
+    const key = JSON.stringify({ start, end });
+
+    logger.debug('checking for existing metrics report for previous month');
+    const metricsForLastMonth = await Export.query().where({ type, key }).first();
+
+    if (metricsForLastMonth) {
+      logger.debug('existing report found, no need to schedule');
+      return;
+    }
+
+    logger.debug('metrics report for previous month not found, scheduling export');
+    return Export.query().insert({ type, key, meta: { start, end }, ready: false });
+  };
+
+  const processExports = () => {
     const start = performance.now();
     return Export.query()
       .where({ ready: false })
       .then(pending => {
-        console.log(`Found ${pending.length} jobs`);
+        logger.info(`Found ${pending.length} jobs`);
         return pending.reduce((promise, row) => {
           return promise
             .then(() => {
@@ -24,17 +51,20 @@ module.exports = settings => {
                 return exporters[row.type](row)
                   .then(result => Export.query().findById(row.id).patch({ ready: true, meta: result }))
                   .catch(err => {
-                    console.error(`Processing failed with error:\n${err.stack}`);
+                    logger.error(`Processing failed with error:\n${err.stack}`);
                   });
               }
-              console.log(`Unrecognised type: ${row.type}`);
+              logger.warn(`Unrecognised type: ${row.type}`);
             });
         }, Promise.resolve());
       })
       .then(() => {
         const time = performance.now() - start;
-        console.log(`Processing took: ${time}ms`);
-      })
-      .then(() => models);
+        logger.info(`Processing took: ${time}ms`);
+      });
   };
+
+  return scheduleTaskMetrics()
+    .then(() => processExports())
+    .then(() => models);
 };
