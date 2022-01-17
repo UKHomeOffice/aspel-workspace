74fe545e0f794fbcaa7f843e19f56337feacb2fa
Phil Moorhouse <phil@wheelsandcogs.co.uk>
2022-01-17 15:29:18 +0000

    extract task metrics scheduler to own job

--- packages/asl-data-exports/lib/worker.js
+++ packages/asl-data-exports/lib/worker.js
@@ -1,5 +1,4 @@
 const { performance } = require('perf_hooks');
-const moment = require('moment');
 const db = require('@asl/schema');
 const rops = require('./rops');
 const taskMetrics = require('./task-metrics');
@@ -18,25 +17,6 @@ module.exports = settings => () => {
   };
 
   const { Export } = models;
-
-  const scheduleTaskMetrics = async () => {
-    const prevMonth = moment().subtract(1, 'month');
-    const start = moment(prevMonth).startOf('month').format('YYYY-MM-DD');
-    const end = moment(prevMonth).endOf('month').format('YYYY-MM-DD');
-    const type = 'task-metrics';
-    const key = JSON.stringify({ start, end });
-
-    logger.debug('checking for existing metrics report for previous month');
-    const metricsForLastMonth = await Export.query().where({ type, key }).first();
-
-    if (metricsForLastMonth) {
-      logger.debug('existing report found, no need to schedule');
-      return;
-    }
-
-    logger.debug('metrics report for previous month not found, scheduling export');
-    return Export.query().insert({ type, key, meta: { start, end }, ready: false });
-  };
 
   const processExports = () => {
     const start = performance.now();
@@ -64,7 +44,6 @@ module.exports = settings => () => {
       });
   };
 
-  return scheduleTaskMetrics()
-    .then(() => processExports())
+  return processExports()
     .then(() => models);
 };
