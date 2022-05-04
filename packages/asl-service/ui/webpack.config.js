2676ae7fd214db76897b025dfcef6ccb7b9dcef7
Leonard Martin <leonard.martin@gmail.com>
2022-05-04 16:47:19 +0100

    Add url polyfill

--- packages/asl-service/ui/webpack.config.js
+++ packages/asl-service/ui/webpack.config.js
@@ -69,7 +69,8 @@ module.exports = dirs => {
       extensions: ['.js', '.jsx'],
       fallback: {
         path: require.resolve('path-browserify'),
-        buffer: require.resolve('buffer/')
+        buffer: require.resolve('buffer/'),
+        url: require.resolve('url/')
       }
     },
     module: {
