4c3903e16ae826847c68be4dfc20c313d335d7f7
Tom Rowsell <rowselltom@gmail.com>
2023-03-30 09:30:19 +0100

    ASL-4247 - copied lint config from react components

--- packages/asl-components/.eslintrc.js
+++ packages/asl-components/.eslintrc.js
@@ -1,18 +1,75 @@
 module.exports = {
-    "env": {
-        "node": true,
-        "browser": true,
-        "commonjs": true,
-        "es2021": true
+    'env': {
+        'node': true,
+        'browser': true,
+        'commonjs': true,
+        'es6': true
     },
-    "extends": "eslint:recommended",
-    "overrides": [
+    'extends': [
+        'eslint:recommended',
+        'plugin:react/recommended'
     ],
-    "parserOptions": {
-        "ecmaVersion": "latest"
+    'parserOptions': {
+        'ecmaVersion': 2020,
+        'sourceType': 'module'
     },
-    "rules": {
-        'implicit-dependencies/no-implicit': 2
+    'settings': {
+        'react': {
+            'version': 'detect'
+        }
     },
-    "plugins" : ["implicit-dependencies"]
-}
+    'rules': {
+        'react/prop-types': 'off',
+        'indent': [
+            'error',
+            4
+            , { 'SwitchCase': 1 }
+        ],
+        'linebreak-style': [
+            'error',
+            'unix'
+        ],
+        'no-unused-vars': [
+            'error',
+            { 'argsIgnorePattern': '^_$'  }
+        ],
+        'quotes': [
+            'error',
+            'single'
+        ],
+        'semi': [
+            'error',
+            'always'
+        ],
+        'object-curly-spacing': [
+            'error', 'always'
+        ],
+        'no-trailing-spaces': [
+            'error'
+        ]
+    },
+    'overrides': [
+        {
+            'files': [ '**/__tests__/**' ],
+            'env': {
+                'jest': true
+            },
+            'globals': {
+                'render': false,
+                'shallow': false,
+                'wrapper': false,
+                'mount': false
+            }
+        },
+        {
+            'files': [ '**/__snapshots__/**' ],
+            'excludedFiles': '*.js.snap'
+        },
+        {
+            'files': [ 'src/**' ],
+            'env': {
+                'commonjs': true
+            }
+        }
+    ]
+};
