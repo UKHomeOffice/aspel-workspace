02267256afeeeac0817972746b599405479bc489
Kiran Varma <Kiran.Varma@digital.homeoffice.gov.uk>
2025-02-13 12:18:28 +0000

    bump: version update (#269)

--- packages/asl-service/ui/assets/js/template.jsx
+++ packages/asl-service/ui/assets/js/template.jsx
@@ -1,4 +1,6 @@
 import React from 'react';
+// todo: remove deprecated hydrate method when React is updated to 18.
+// eslint-disable-next-line react/no-deprecated
 import { hydrate } from 'react-dom';
 import { Provider } from 'react-redux';
 import { Wrapper } from '@ukhomeoffice/asl-components';
