08dddf52094865018136ae04b8f75b0d67c90f2d
kiran-varma-home-office <kiran.varma@digital.homeoffice.gov.uk>
2025-05-07 16:53:28 +0100

    mandatory training cleanup

--- packages/asl-pages/pages/role/named-person-mvp/mandatory-training/index.js
+++ packages/asl-pages/pages/role/named-person-mvp/mandatory-training/index.js
@@ -1,36 +1,7 @@
-const { get, merge } = require('lodash');
 const { page } = require('@asl/service/ui');
 const { form } = require('../../../common/routers');
 const { buildModel } = require('../../../../lib/utils');
 const schema = require('./schema');
-const confirm = require('../confirm');
-const success = require('../routers/success');
-const { profileReplaced, PELH_OR_NPRC_ROLES } = require('../../helper');
-const { populateNamedPeople } = require('../../../common/middleware');
-
-const sendData = (req, params = {}) => {
-  const { type, rcvsNumber, comment } = req.session.form[req.model.id].values;
-
-  const replaceProfile = profileReplaced(req.establishment, type);
-  const opts = {
-    method: 'POST',
-    json: merge(
-      {
-        data: {
-          type,
-          rcvsNumber,
-          profileId: req.profileId,
-          replaceProfile,
-          replaceRoles: PELH_OR_NPRC_ROLES
-        },
-        meta: { comment }
-      },
-      params
-    )
-  };
-
-  return req.api(`/establishment/${req.establishmentId}/role`, opts);
-};
 
 module.exports = (settings) => {
   const app = page({
@@ -83,29 +54,6 @@ module.exports = (settings) => {
       return res.redirect(req.buildRoute('training.dashboard'));
     }
   });
-
-  app.use(
-    '/confirm',
-    populateNamedPeople,
-    confirm({
-      action: 'create',
-      sendData
-    })
-  );
-
-  app.post('/confirm', populateNamedPeople, (req, res, next) => {
-    sendData(req)
-      .then((response) => {
-        req.session.success = { taskId: get(response, 'json.data.id') };
-        delete req.session.form[req.model.id];
-        return res.redirect(
-          req.buildRoute('role.create', { suffix: 'success' })
-        );
-      })
-      .catch(next);
-  });
-
-  app.use('/success', success());
 
   return app;
 };
