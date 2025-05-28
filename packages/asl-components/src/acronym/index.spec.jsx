36eed4eef9a7f4c29934e3642207880fe012e9cd
farrukhishaq <farrukh.ishaq@homeoffice.gov.uk>
2025-05-28 12:43:44 +0100

    acronym tested with RTL.

--- packages/asl-components/src/acronym/index.spec.jsx
+++ packages/asl-components/src/acronym/index.spec.jsx
@@ -1,39 +1,39 @@
 import React from 'react';
-import { render } from 'enzyme';
+import { render, screen } from '@testing-library/react';
 import Acronym from './';
 import dictionary from '@ukhomeoffice/asl-dictionary';
 
 describe('Acronym component', () => {
   Object.entries(dictionary)
-      .filter(([,definition]) => typeof definition === "string")
-      .forEach(([key, definition]) => {
-    test(`handles input ${key}`, () => {
-      const wrapper = render(<Acronym>{key}</Acronym>);
-      expect(wrapper.attr('title')).toBe(definition);
-      expect(wrapper.text()).toBe(key);
-      expect(wrapper.get(0).tagName).toBe('abbr');
+    .filter(([, definition]) => typeof definition === 'string')
+    .forEach(([key, definition]) => {
+      test(`handles input ${key}`, () => {
+        render(<Acronym>{key}</Acronym>);
+        const element = screen.getByText(key);
+        expect(element).toHaveAttribute('title', definition);
+        expect(element.tagName).toBe('ABBR');
+      });
     });
-  });
 
   Object.entries(dictionary.plural)
-      .filter(([,definition]) => typeof definition === "string")
-      .forEach(([key, definition]) => {
-    test(`handles input ${key} and outputs a plural definition`, () => {
-      const wrapper = render(<Acronym usePlural>{key}</Acronym>);
-      expect(wrapper.attr('title')).toBe(definition);
-      expect(wrapper.text()).toBe(key);
-      expect(wrapper.get(0).tagName).toBe('abbr');
+    .filter(([, definition]) => typeof definition === 'string')
+    .forEach(([key, definition]) => {
+      test(`handles input ${key} and outputs a plural definition`, () => {
+        render(<Acronym usePlural>{key}</Acronym>);
+        const element = screen.getByText(key);
+        expect(element).toHaveAttribute('title', definition);
+        expect(element.tagName).toBe('ABBR');
+      });
     });
-  });
 
   Object.entries(dictionary)
-      .filter(([key,definition]) => typeof definition === "string" && dictionary.plural[key] === undefined)
-      .forEach(([key, definition]) => {
-    test(`handles input ${key} and fallsback to singular when no plural defined`, () => {
-      const wrapper = render(<Acronym usePlural>{key}</Acronym>);
-      expect(wrapper.attr('title')).toBe(definition);
-      expect(wrapper.text()).toBe(key);
-      expect(wrapper.get(0).tagName).toBe('abbr');
+    .filter(([key, definition]) => typeof definition === 'string' && dictionary.plural[key] === undefined)
+    .forEach(([key, definition]) => {
+      test(`handles input ${key} and falls back to singular when no plural defined`, () => {
+        render(<Acronym usePlural>{key}</Acronym>);
+        const element = screen.getByText(key);
+        expect(element).toHaveAttribute('title', definition);
+        expect(element.tagName).toBe('ABBR');
+      });
     });
-  });
 });
