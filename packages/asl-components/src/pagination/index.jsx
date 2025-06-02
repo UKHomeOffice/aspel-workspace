2b2a7735efbbb9f99628261d883c74ab8b147218
Jeff Horton <jeff.horton@digital.homeoffice.gov.uk>
2025-06-02 18:16:36 +0100

    [ASL-4671] Remove link to current page if there is only 1 page

--- packages/asl-components/src/pagination/index.jsx
+++ packages/asl-components/src/pagination/index.jsx
@@ -1,8 +1,17 @@
-import React, { Fragment } from 'react';
+import React, { Fragment, useMemo } from 'react';
 import classnames from 'classnames';
 import { connect } from 'react-redux';
 import { ApplyChanges } from '../';
 import { changePage } from './actions';
+
+const pagesToShow = (page, totalPages) => {
+    if(totalPages <= 1) {
+        return [];
+    }
+    const end = Math.min(totalPages, Math.max(5, page + 3));
+    const start = Math.max(0, end - 5);
+    return Array.from(Array(totalPages).keys()).slice(start, end);
+};
 
 export const Pagination = ({
     totalPages,
@@ -11,40 +20,40 @@ export const Pagination = ({
     count,
     onPageChange
 }) => {
-    const pagesToShow = () => {
-        const end = Math.min(totalPages, Math.max(5, page + 3));
-        const start = Math.max(0, end - 5);
-        return Array.from(Array(totalPages).keys()).slice(start, end);
-    };
-    const links = [
-        ...(page > 0
-            ? [{
-                ariaLabel: 'Previous page',
-                className: 'prev',
-                label: <Fragment><span aria-hidden="true" role="presentation">&laquo;</span> Previous</Fragment>,
-                target: (page - 1) || 0
-            }]
-            : []
-        ),
-        ...pagesToShow().map(p => ({
-            ariaLabel: `Page ${p + 1}`,
-            label: p + 1,
-            target: p,
-            disabled: page === p
-        })),
-        ...(page < totalPages - 1
-            ? [{
-                ariaLabel: 'Next page',
-                className: 'next',
-                label: <Fragment>Next <span aria-hidden="true" role="presentation">&raquo;</span></Fragment>,
-                target: page + 1,
-            }]
-            : []
-        )
-    ];
+    const links = useMemo(
+        () => [
+            ...(page > 0
+                ? [{
+                    ariaLabel: 'Previous page',
+                    className: 'prev',
+                    label: <Fragment><span aria-hidden="true" role="presentation">&laquo;</span> Previous</Fragment>,
+                    target: (page - 1) || 0
+                }]
+                : []
+            ),
+            ...pagesToShow(page, totalPages).map(p => ({
+                ariaLabel: `Page ${p + 1}`,
+                label: p + 1,
+                target: p,
+                disabled: page === p
+            })),
+            ...(page < totalPages - 1
+                ? [{
+                    ariaLabel: 'Next page',
+                    className: 'next',
+                    label: <Fragment>Next <span aria-hidden="true" role="presentation">&raquo;</span></Fragment>,
+                    target: page + 1
+                }]
+                : []
+            )
+        ],
+        [page, totalPages]
+    );
     return (
         <nav className="pagination" role="navigation" aria-label="Pagination">
-            <div className="pagination-summary">{`Showing ${count ? page * limit + 1 : 0} – ${(page + 1) * limit < count ? (page + 1) * limit : count} of ${count} results`}</div>
+            <div
+                className="pagination-summary">{`Showing ${count ? page * limit + 1 : 0} – ${(page + 1) * limit < count ? (page + 1) * limit : count} of ${count} results`}
+            </div>
             <ul>
                 {
                     links.map((link, key) =>
