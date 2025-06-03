9fc49b8cb867d2e181741663077e340907bce8f1
kiran-varma-home-office <kiran.varma@digital.homeoffice.gov.uk>
2025-06-03 17:12:57 +0100

    call establishment api

--- packages/asl-pages/pages/role/named-person-mvp/confirm/index.js
+++ packages/asl-pages/pages/role/named-person-mvp/confirm/index.js
@@ -1,10 +1,35 @@
+const { get, merge } = require('lodash');
 const { page } = require('@asl/service/ui');
 const form = require('../../../common/routers/form');
-const {
-  updateDataFromTask,
-  redirectToTaskIfOpen,
-  populateNamedPeople
-} = require('../../../common/middleware');
+const { populateNamedPeople } = require('../../../common/middleware');
+const { profileReplaced, PELH_OR_NPRC_ROLES } = require('../../helper');
+
+const sendData = (req, params = {}) => {
+  // eslint-disable-next-line no-warning-comments
+  //TODO: get nvs number and comment when working on nvs journey
+  const { type, rcvsNumber, comment } =
+    req.session.form[`${req.profileId}-new-role-named-person`].values;
+
+  const replaceProfile = profileReplaced(req.establishment, type);
+  const opts = {
+    method: 'POST',
+    json: merge(
+      {
+        data: {
+          type,
+          rcvsNumber,
+          profileId: req.profileId,
+          replaceProfile,
+          replaceRoles: PELH_OR_NPRC_ROLES
+        },
+        meta: { comment }
+      },
+      params
+    )
+  };
+
+  return req.api(`/establishment/${req.establishmentId}/role`, opts);
+};
 
 module.exports = (settings) => {
   const app = page({
@@ -40,14 +65,17 @@ module.exports = (settings) => {
     })
   );
 
-  app.post(
-    '/',
-    updateDataFromTask(settings.sendData),
-    redirectToTaskIfOpen(),
-    (req, res, next) => {
-      return res.redirect(req.buildRoute('role.namedPersonMvp.success'));
-    }
-  );
+  app.post('/', populateNamedPeople, (req, res, next) => {
+    sendData(req)
+      .then((response) => {
+        req.session.success = { taskId: get(response, 'json.data.id') };
+        Object.keys(req.session.form).forEach((entry) => {
+          delete req.session.form[entry];
+        });
+        return res.redirect(req.buildRoute('role.namedPersonMvp.success'));
+      })
+      .catch(next);
+  });
 
   return app;
 };
