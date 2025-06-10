af1aefd6785237b7554c9d9d36e3cdf9d4c7179b
Jeff Horton <jeff.horton@digital.homeoffice.gov.uk>
2025-06-10 16:23:45 +0100

    [ASL-4718] Remove debug code

--- packages/asl-notifications/lib/recipients/establishment.js
+++ packages/asl-notifications/lib/recipients/establishment.js
@@ -31,8 +31,6 @@ module.exports = async ({ schema, logger, task }) => {
     role: ['create', 'delete']
   };
 
-  console.log({action, model, task});
-
   if (!allowedActions[model].includes(action)) {
     logger.verbose(`ignoring task: ${model} ${action}`);
     return Promise.resolve(new Map());
