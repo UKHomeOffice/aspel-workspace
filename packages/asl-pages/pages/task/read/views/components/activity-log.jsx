8720f76509ebcefe7707a67482d1d851afb7cdef
kiran-varma-home-office <kiran.varma@digital.homeoffice.gov.uk>
2025-07-16 12:57:58 +0100

    use version instead of np faeture flag

--- packages/asl-pages/pages/task/read/views/components/activity-log.jsx
+++ packages/asl-pages/pages/task/read/views/components/activity-log.jsx
@@ -12,8 +12,7 @@ import {
   isTrueish
 } from '../../../../../lib/utils';
 import PplDeclarations from './ppl-declarations';
-import { useFeatureFlag } from '@asl/service/ui/feature-flag';
-const { featureFlags } = require('@ukhomeoffice/asl-constants');
+const { versions } = require('@ukhomeoffice/asl-constants');
 
 function ProfileLink({ id, name, establishmentId, asruUser }) {
   if (establishmentId && !asruUser) {
@@ -358,7 +357,7 @@ function LogItem({ item, task }) {
   const isAssignment = item.eventName === 'assign';
   const isIntentionToRefuse = action === 'intention-to-refuse';
   const roleData = task.data.data;
-  const namedPersonFeatureFlag = useFeatureFlag(featureFlags.FEATURE_FLAG_NAMED_PERSON_MVP);
+  const { version } = task.data.meta;
 
   if (action === 'update') {
     if (isExtension) {
@@ -378,7 +377,7 @@ function LogItem({ item, task }) {
         activity={item}
         changedBy={item.changedBy}
       />
-      { namedPersonFeatureFlag && <Snippet fallback='declarations.default'>declarations.{roleData.type}</Snippet> }
+      { version === versions.NAMED_PERSON_VERSION_ID && <Snippet fallback='declarations.default'>declarations.{roleData.type}</Snippet> }
       <InspectorRecommendation item={item} />
       {isExtension && <DeadlineDetails item={item} />}
       {isRa && <AwerbDate item={item} />}
