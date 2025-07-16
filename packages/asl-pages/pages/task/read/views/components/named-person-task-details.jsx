8b305bc04f06f557c49915704c899250a24d110e
kiran-varma-home-office <kiran.varma@digital.homeoffice.gov.uk>
2025-07-16 10:05:02 +0100

    don't show training summary and mandatory for other roles

--- packages/asl-pages/pages/task/read/views/components/named-person-task-details.jsx
+++ packages/asl-pages/pages/task/read/views/components/named-person-task-details.jsx
@@ -15,21 +15,26 @@ export const NamedPersonTaskDetails = ({ taskData, profile }) => {
         <NamedPersonDetails roleType={taskData.type} profile={profile} />
         <DetailsByRole incompleteTraining={taskData} mandatoryTraining={taskData.mandatory} role={taskData.type} roleDetails={taskData} />
       </div>
-      <div className="sticky-nav-anchor">
-        <TrainingSummary certificates={profile.certificates} />
-      </div>
-      <div>
-        <Details
-          summary={`${taskData.type.toUpperCase()} mandatory training requirements`}
-          className="margin-bottom"
-          id="mandatory-training-summary"
-          role="group"
-        >
-          <Inset>
-            <MandatoryTrainingRequirements roleType={taskData.type} />
-          </Inset>
-        </Details>
-      </div>
+
+      {['nawco', 'nvs'].includes(taskData.type) && (
+        <>
+          <div className="sticky-nav-anchor">
+            <TrainingSummary certificates={profile.certificates} />
+          </div>
+          <div>
+            <Details
+              summary={`${taskData.type.toUpperCase()} mandatory training requirements`}
+              className="margin-bottom"
+              id="mandatory-training-summary"
+              role="group"
+            >
+              <Inset>
+                <MandatoryTrainingRequirements roleType={taskData.type} />
+              </Inset>
+            </Details>
+          </div>
+        </>
+      )}
     </dl>
   );
 };
