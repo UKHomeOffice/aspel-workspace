cfd92210ae6ce5b560f2a293aa3e4ac05f371bce
kiran-varma-home-office <kiran.varma@digital.homeoffice.gov.uk>
2025-09-01 16:28:24 +0100

    cleanup

--- packages/asl-notifications/jobs/training-due-reminder.js
+++ packages/asl-notifications/jobs/training-due-reminder.js
@@ -6,7 +6,6 @@ const { query } = require('../../asl-schema/schema/base-model');
 const buildRoleQueryWithCompletionDate = ({ Role, completeDate }) => {
   return Role.query()
     .select('roles.profileId', 'roles.establishmentId', 'roles.type', 'roles.trainingDelayDetails', 'profile.first_name', 'profile.last_name', 'establishment.name')
-    // .whereRaw(`training_delay_details->>'completeDate' = '2026-01-01'`)
     .joinRelated('profile')
     .joinRelated('establishment')
     .whereRaw(`training_delay_details->>'completeDate' = '${completeDate}'`)
@@ -17,7 +16,7 @@ module.exports = async ({ schema, logger, publicUrl }) => {
   const emailer = Emailer({ schema, logger, publicUrl });
 
   //TODO: Remove before merge
-  const roleQuery = buildRoleQueryWithCompletionDate({ Role, completeDate: '2026-01-01', emailer, logger });
+  // const roleQuery = buildRoleQueryWithCompletionDate({ Role, completeDate: '2026-01-01', emailer, logger });
   const notify3Months = buildRoleQueryWithCompletionDate({ Role, completeDate: moment().add(3, 'months').format('YYYY-MM-DD'), emailer });
   const notify1Months = buildRoleQueryWithCompletionDate({ Role, completeDate: moment().add(1, 'months').format('YYYY-MM-DD'), emailer });
   const notify1day = buildRoleQueryWithCompletionDate({ Role, completeDate: moment().add(1, 'days').format('YYYY-MM-DD'), emailer });
@@ -27,7 +26,6 @@ module.exports = async ({ schema, logger, publicUrl }) => {
 
   logger.debug(`Found ${rolesWithTrainingOutstanding.length} roles with training due in 3 months`);
 
-  // return Promise.all(rolesWithTrainingOutstanding.flat().map(role => console.log(role)));
   return Promise.all(rolesWithTrainingOutstanding.flat().map(role => {
     emailer({
       event: 'direct-notification',
