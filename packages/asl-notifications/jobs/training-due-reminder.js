af1e8b42e67526a653f94a408e52218877b79265
kiran-varma-home-office <kiran.varma@digital.homeoffice.gov.uk>
2025-09-12 15:03:06 +0100

    use moment to format date

--- packages/asl-notifications/jobs/training-due-reminder.js
+++ packages/asl-notifications/jobs/training-due-reminder.js
@@ -1,6 +1,5 @@
 const moment = require('moment');
 const Emailer = require('../lib/emailer');
-const { format: dateFormatter } = require('date-fns');
 
 const buildRoleQueryWithCompletionDate = ({ Role, dates }) => {
   return Role.query()
@@ -53,7 +52,7 @@ module.exports = async ({ schema, logger, publicUrl }) => {
           firstName: role.firstName,
           lastName: role.lastName,
           name: role.name,
-          completeDate: dateFormatter(role.trainingDelayDetails.completeDate, 'dd MMMM yyyy')
+          completeDate: moment(role.trainingDelayDetails.completeDate).format('DD MMM YYYY')
         }
       }
     })));
