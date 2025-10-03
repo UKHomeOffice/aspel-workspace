3ffd525eed7861d951e6dca3609193bff70f2b03
farrukhishaq <farrukh.ishaq@homeoffice.gov.uk>
2025-10-03 11:15:35 +0100

    replaceHBA now show meta about in all places where HBA and has replaced.

---
+++ packages/asl-pages/pages/project/read/views/components/hba.jsx
@@ -0,0 +1,41 @@
+import React from 'react';
+import { Link, Snippet } from '@ukhomeoffice/asl-components';
+import {format as formatDate} from 'date-fns';
+import { dateFormat } from '@asl/pages/constants';
+
+const HBA = ({ version, project, canReplaceHBA, hbaHeading }) => {
+  if (!version?.hbaToken) {
+    return null;
+  }
+
+  return (
+    <div>
+      {hbaHeading && (
+        <h3>
+          <Snippet>downloads.hba.heading</Snippet>
+        </h3>
+      )}
+
+      <p>
+        <a href={`/attachment/${version.hbaToken}`} download={version.hbaFilename}>
+          <Snippet>otherDocuments.links.hba</Snippet>
+        </a>
+
+        {canReplaceHBA && (
+          <Link
+            page="project.replaceHba"
+            project={project}
+            className="govuk-!-padding-left-3"
+            label={<Snippet>otherDocuments.links.replaceHba</Snippet>}
+          />
+        )}
+      </p>
+
+      {version.hbaReplaced && (
+        <span>Replaced {formatDate(version.updatedAt, dateFormat.datetime)}</span>
+      )}
+    </div>
+  );
+};
+
+export default HBA;
