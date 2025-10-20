bd72c6b9406053148fbf639ab0b8dedfe36cacff
Parvin Baishkiyar <Parvin.Baishkiyar@digital.homeoffice.gov.uk>
2025-10-20 09:44:39 +0100

    asl-4899-Adding-new-step-to-existing-protocol-shows-change-badge-for-new-and-existing-step (#257)

--- packages/asl-projects/client/components/changed-badge.js
+++ packages/asl-projects/client/components/changed-badge.js
@@ -9,8 +9,31 @@ export default function ChangedBadge({ fields = [], changedFromGranted, changedF
   const previousProtocols = useSelector(state => state.application?.previousProtocols || {}, shallowEqual);
 
   const changedFrom = source => {
-    return source.length && fields.some(field => {
-      return source.some(change => minimatch(change, field));
+    //Protocol, removing config fields, which causing false change badge showing up
+    let cleanedSource;
+    if (protocolId) {
+      cleanedSource = source.filter(item =>
+        !item.endsWith('usedInProtocols') &&
+        !item.endsWith('reusedStep') &&
+        !item.endsWith('reusableStepId') &&
+        !item.endsWith('usedInProtocols.protocolId') &&
+        !item.endsWith('usedInProtocols.protocolNumber')
+      );
+      cleanedSource = cleanedSource.filter((item, _, arr) => {
+        // Check if it ends with any of the fields
+        const endsWithTarget = fields.some(field => item.endsWith(field));
+
+        // Check if this item is a substring of any other item in the array
+        const isContainedElsewhere = arr.some(other => other !== item && other.includes(item));
+
+        // Keep it if it doesn't end with field OR it's contained elsewhere
+        return !endsWithTarget || isContainedElsewhere;
+      });
+    } else {
+      cleanedSource = source;
+    }
+    return cleanedSource.length && fields.some(field => {
+      return cleanedSource.some(change => minimatch(change, field));
     });
   };
 
