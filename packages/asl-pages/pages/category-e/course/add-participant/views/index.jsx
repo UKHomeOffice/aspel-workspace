be6483a2d0a27a53321cec0a8370731fcd631be3
Jeff Horton <jeff.horton@digital.homeoffice.gov.uk>
2025-11-04 14:35:48 +0000

    [Chore] Correct index page names

--- packages/asl-pages/pages/category-e/course/add-participant/views/index.jsx
+++ packages/asl-pages/pages/category-e/course/add-participant/views/index.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Header, Snippet } from '@ukhomeoffice/asl-components';
 
-export default function RescheduleCoursePage() {
+export default function AddCourseParticipantPage() {
   return <>
     <Header
       title={<Snippet>pageTitle</Snippet>}
