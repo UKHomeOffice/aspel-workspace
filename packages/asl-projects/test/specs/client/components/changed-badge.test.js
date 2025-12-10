54c3d4342bc95577a1a884780f53c2129e5a954f
kiran-varma-home-office <kiran.varma@digital.homeoffice.gov.uk>
2025-12-10 18:11:06 +0000

    update to use equal in tests

--- packages/asl-projects/test/specs/client/components/changed-badge.test.js
+++ packages/asl-projects/test/specs/client/components/changed-badge.test.js
@@ -4,37 +4,37 @@ import { hasMatchingChange } from '../../../../client/components/changed-badge';
 describe('ChangedBadge', () => {
   describe('hasMatchingChange', () => {
     it('returns false if changes list is empty', () => {
-      assert.strictEqual(hasMatchingChange(['field1'], []), false);
-      assert.strictEqual(hasMatchingChange(['field1'], null), false);
-      assert.strictEqual(hasMatchingChange(['field1'], undefined), false);
+      assert.equal(hasMatchingChange(['field1'], []), false);
+      assert.equal(hasMatchingChange(['field1'], null), false);
+      assert.equal(hasMatchingChange(['field1'], undefined), false);
     });
 
     it('returns false if fields list is empty', () => {
-      assert.strictEqual(hasMatchingChange([], ['field1']), false);
-      assert.strictEqual(hasMatchingChange(null, ['field1']), false);
-      assert.strictEqual(hasMatchingChange(undefined, ['field1']), false);
+      assert.equal(hasMatchingChange([], ['field1']), false);
+      assert.equal(hasMatchingChange(null, ['field1']), false);
+      assert.equal(hasMatchingChange(undefined, ['field1']), false);
     });
 
     it('returns true if there is an exact match', () => {
       const fields = ['field1', 'field2'];
       const changes = ['field2', 'field3'];
-      assert.strictEqual(hasMatchingChange(fields, changes), true);
+      assert.equal(hasMatchingChange(fields, changes), true);
     });
 
     it('returns true if there is a wildcard match', () => {
       const fields = ['protocols.*.title'];
       const changes = ['protocols.123.title'];
-      assert.strictEqual(hasMatchingChange(fields, changes), true);
+      assert.equal(hasMatchingChange(fields, changes), true);
     });
 
     it('returns false if there are no matches', () => {
       const fields = ['field1', 'field2'];
       const changes = ['field3', 'field4'];
-      assert.strictEqual(hasMatchingChange(fields, changes), false);
+      assert.equal(hasMatchingChange(fields, changes), false);
 
       const fields2 = ['protocols.123.title'];
       const changes2 = ['protocols.*.description'];
-      assert.strictEqual(hasMatchingChange(fields2, changes2), false);
+      assert.equal(hasMatchingChange(fields2, changes2), false);
     });
   });
 });
