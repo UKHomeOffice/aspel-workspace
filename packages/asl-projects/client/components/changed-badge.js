a6d9afe5a48c8d48accaf5fcf899da4fa85f60ae
kiran-varma-home-office <kiran.varma@digital.homeoffice.gov.uk>
2025-12-10 15:17:37 +0000

    refactor and add tests

--- packages/asl-projects/client/components/changed-badge.js
+++ packages/asl-projects/client/components/changed-badge.js
@@ -2,37 +2,37 @@ import React from 'react';
 import { useSelector, shallowEqual } from 'react-redux';
 import minimatch from 'minimatch';
 
+export const hasMatchingChange = (fields, changes) => {
+  if (!changes?.length || !fields?.length) {
+    return false;
+  }
+
+  for (const field of fields) {
+    for (const change of changes) {
+      if (minimatch(change, field)) {
+        return true;
+      }
+    }
+  }
+
+  return false;
+};
+
 export default function ChangedBadge({ fields = [], changedFromGranted, changedFromLatest, changedFromFirst, protocolId, noLabel }) {
   const latest = useSelector(state => state.changes?.latest || [], shallowEqual);
   const granted = useSelector(state => state.changes?.granted || [], shallowEqual);
   const first = useSelector(state => state.changes?.first || [], shallowEqual);
   const previousProtocols = useSelector(state => state.application?.previousProtocols || {}, shallowEqual);
 
-  const hasMatchingChange = (changes) => {
-    if (!changes?.length || !fields?.length) {
-      return false;
-    }
-
-    for (const field of fields) {
-      for (const change of changes) {
-        if (minimatch(change, field)) {
-          return true;
-        }
-      }
-    }
-
-    return false;
-  };
-
   const protocolAllowed = (list) => !protocolId || list?.includes(protocolId);
 
-  if ((changedFromLatest || hasMatchingChange(latest)) && protocolAllowed(previousProtocols.previous)) {
+  if ((changedFromLatest || hasMatchingChange(fields, latest)) && protocolAllowed(previousProtocols.previous)) {
     return <span className="badge changed">{noLabel ? '' : 'changed'}</span>;
   }
-  if ((changedFromGranted || hasMatchingChange(granted)) && protocolAllowed(previousProtocols.granted)) {
+  if ((changedFromGranted || hasMatchingChange(fields, granted)) && protocolAllowed(previousProtocols.granted)) {
     return <span className="badge">{noLabel ? '' : 'amended'}</span>;
   }
-  if ((changedFromFirst || hasMatchingChange(first)) && protocolAllowed(previousProtocols.first)) {
+  if ((changedFromFirst || hasMatchingChange(fields, first)) && protocolAllowed(previousProtocols.first)) {
     return <span className="badge">{noLabel ? '' : 'changed'}</span>;
   }
 
