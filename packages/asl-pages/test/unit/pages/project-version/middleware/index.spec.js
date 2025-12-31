ea27c91e7a82cbb6b383a693f75b59797f3e97f4
Jeff Horton <jeff.horton@digital.homeoffice.gov.uk>
2025-12-31 16:45:49 +0000

    [ASL-4987] make version selection reletive to current version

---
+++ packages/asl-pages/test/unit/pages/project-version/middleware/index.spec.js
@@ -0,0 +1,134 @@
+import { getVersionsForDiff } from '../../../../../pages/project-version/middleware/index';
+
+describe('Versions', () => {
+  describe('Getting relevant versions for change detection', () => {
+
+    const buildProjectVersionsReq = (versions, currentId) => {
+      return {
+        versionId: currentId,
+        project: { versions }
+      };
+    };
+
+    it('can find the correct of two versions before a project is granted', () => {
+      const versions = [
+        { 'id': 'current', 'status': 'draft', createdAt: '2025-12-31T12:34:45.000Z' },
+        { 'id': 'previous', 'status': 'submitted', createdAt: '2025-12-30T12:34:45.000Z' }
+      ];
+
+      const { previous, first, granted } = getVersionsForDiff(
+        buildProjectVersionsReq(versions, 'current')
+      );
+
+      expect(previous).toBe(versions[1]);
+      expect(first).toBeUndefined();
+      expect(granted).toBeUndefined();
+    });
+
+    it('can find the correct of four versions before a project is granted', () => {
+      const versions = [
+        { 'id': 'current', 'status': 'draft', createdAt: '2025-12-31T12:34:45.000Z' },
+        { 'id': 'previous', 'status': 'submitted', createdAt: '2025-12-30T12:34:45.000Z' },
+        { 'id': 'older', 'status': 'submitted', createdAt: '2025-12-29T12:34:45.000Z' },
+        { 'id': 'first', 'status': 'submitted', createdAt: '2025-12-28T12:34:45.000Z' }
+      ];
+
+      const { previous, first, granted } = getVersionsForDiff(
+        buildProjectVersionsReq(versions, 'current')
+      );
+
+      expect(previous).toBe(versions[1]);
+      expect(first).toBe(versions[3]);
+      expect(granted).toBeUndefined();
+    });
+
+    it('can find the correct versions once a project is granted', () => {
+      const versions = [
+        { 'id': 'current', 'status': 'granted', createdAt: '2025-12-31T12:34:45.000Z' },
+        { 'id': 'previous', 'status': 'submitted', createdAt: '2025-12-30T12:34:45.000Z' },
+        { 'id': 'older', 'status': 'submitted', createdAt: '2025-12-29T12:34:45.000Z' },
+        { 'id': 'first', 'status': 'submitted', createdAt: '2025-12-28T12:34:45.000Z' }
+      ];
+
+      const { previous, first, granted } = getVersionsForDiff(
+        buildProjectVersionsReq(versions, 'current')
+      );
+
+      expect(previous).toBe(versions[1]);
+      expect(first).toBe(versions[3]);
+      expect(granted).toBeUndefined();
+    });
+
+    it('can find the correct versions for a draft amendment', () => {
+      const versions = [
+        { 'id': 'current', 'status': 'draft', createdAt: '2025-12-31T12:34:45.000Z' },
+        { 'id': 'previous', 'status': 'submitted', createdAt: '2025-12-30T12:34:45.000Z' },
+        { 'id': 'previous-granted', 'status': 'granted', createdAt: '2025-12-29T12:34:45.000Z' },
+        { 'id': 'first', 'status': 'submitted', createdAt: '2025-12-28T12:34:45.000Z' }
+      ];
+
+      const { previous, first, granted } = getVersionsForDiff(
+        buildProjectVersionsReq(versions, 'current')
+      );
+
+      expect(previous).toBe(versions[1]);
+      expect(first).toBeUndefined();
+      expect(granted).toBe(versions[2]);
+    });
+
+    it('can find the correct versions for a granted amendment', () => {
+      const versions = [
+        { 'id': 'current', 'status': 'granted', createdAt: '2025-12-31T12:34:45.000Z' },
+        { 'id': 'previous', 'status': 'submitted', createdAt: '2025-12-30T12:34:45.000Z' },
+        { 'id': 'previous-granted', 'status': 'granted', createdAt: '2025-12-29T12:34:45.000Z' },
+        { 'id': 'first', 'status': 'submitted', createdAt: '2025-12-28T12:34:45.000Z' }
+      ];
+
+      const { previous, first, granted } = getVersionsForDiff(
+        buildProjectVersionsReq(versions, 'current')
+      );
+
+      expect(previous).toBe(versions[1]);
+      expect(first).toBeUndefined();
+      expect(granted).toBe(versions[2]);
+    });
+
+    it('can find the correct versions for an archived task once a project is granted', () => {
+      const versions = [
+        { 'id': 'newer-granted', 'status': 'granted', createdAt: '2026-01-02T12:34:45.000Z' },
+        { 'id': 'newer-submitted', 'status': 'submitted', createdAt: '2026-01-02T12:34:45.000Z' },
+        { 'id': 'current', 'status': 'granted', createdAt: '2025-12-31T12:34:45.000Z' },
+        { 'id': 'previous', 'status': 'submitted', createdAt: '2025-12-30T12:34:45.000Z' },
+        { 'id': 'older', 'status': 'submitted', createdAt: '2025-12-29T12:34:45.000Z' },
+        { 'id': 'first', 'status': 'submitted', createdAt: '2025-12-28T12:34:45.000Z' }
+      ];
+
+      const { previous, first, granted } = getVersionsForDiff(
+        buildProjectVersionsReq(versions, 'current')
+      );
+
+      expect(previous).toBe(versions[3]);
+      expect(first).toBe(versions[5]);
+      expect(granted).toBeUndefined();
+    });
+
+    it('can find the correct versions for an archived task once an amendment is granted', () => {
+      const versions = [
+        { 'id': 'newer-granted', 'status': 'granted', createdAt: '2026-01-02T12:34:45.000Z' },
+        { 'id': 'newer-submitted', 'status': 'submitted', createdAt: '2026-01-02T12:34:45.000Z' },
+        { 'id': 'current', 'status': 'granted', createdAt: '2025-12-31T12:34:45.000Z' },
+        { 'id': 'previous', 'status': 'submitted', createdAt: '2025-12-30T12:34:45.000Z' },
+        { 'id': 'previous-granted', 'status': 'granted', createdAt: '2025-12-29T12:34:45.000Z' },
+        { 'id': 'first', 'status': 'submitted', createdAt: '2025-12-28T12:34:45.000Z' }
+      ];
+
+      const { previous, first, granted } = getVersionsForDiff(
+        buildProjectVersionsReq(versions, 'current')
+      );
+
+      expect(previous).toBe(versions[3]);
+      expect(first).toBeUndefined();
+      expect(granted).toBe(versions[4]);
+    });
+  });
+});
