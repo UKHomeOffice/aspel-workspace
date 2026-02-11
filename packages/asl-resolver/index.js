9d5dac988cf7c3b4e4028cb5dc09ebdb10613424
Parvin Baishkiyar <Parvin.Baishkiyar@digital.homeoffice.gov.uk>
2026-02-11 11:17:02 +0000

    Feat/aws sdk v2 to v3 (#333)
    * pushing initial code
    
    * created & approved a project..
    
    * adding old comment back
    
    * removing endpoint
    
    * making workflow SQSClient work with local stack
    
    * tested asl-search
    
    * test cases failure
    
    * resolving some audit issues
    
    * adding exception for scan image issues
    
    * Update packages/asl-search/lib/elasticsearch/index.js
    
    Co-authored-by: Jeff Horton <87995501+jeff-horton-ho-sas@users.noreply.github.com>
    
    * no longer needed based on Jeff suggested code
    
    * Revert "adding exception for scan image issues"
    
    This reverts commit c7b39c44598694260550a69b30555760617a3e34.
    
    * Revert "resolving some audit issues"
    
    This reverts commit 0b693f8fa9fcd490726cffebf08da519a3033d77.
    
    * resolving audit issues
    
    * audit fix
    
    ---------
    
    Co-authored-by: Jeff Horton <87995501+jeff-horton-ho-sas@users.noreply.github.com>

--- packages/asl-resolver/index.js
+++ packages/asl-resolver/index.js
@@ -1,30 +1,45 @@
 const { Consumer } = require('sqs-consumer');
-const AWS = require('aws-sdk');
+const { SQSClient } = require('@aws-sdk/client-sqs');
 const config = require('./config');
-
 const Logger = require('./lib/utils/logger');
 
 const logger = Logger(config);
 
 const handleMessage = require('./lib/worker')({ ...config, logger });
 
-const sqs = new AWS.SQS({
+const configParams = {
   region: config.sqs.region,
   accessKeyId: config.sqs.accessKey,
   secretAccessKey: config.sqs.secret
-});
+};
+if (config.s3.localstackUrl) {
+  configParams.endpoint = config.s3.localstackUrl;
+  configParams.sslEnabled = false;
+}
 
+// Create SQS client using AWS SDK v3
+const sqsClient = new SQSClient(configParams);
+
+// Initialize SQS consumer
 const app = Consumer.create({
   queueUrl: config.sqs.url,
   handleMessage,
   batchSize: 10,
-  sqs
+  sqs: sqsClient
 });
 
 app.on('error', error => {
   logger.error(error.message);
   app.stop();
   setTimeout(() => app.start(), 1000);
+});
+
+app.on('processing_error', (err) => {
+  console.error(err.message);
+});
+
+app.on('timeout_error', (err) => {
+  console.error(err.message);
 });
 
 app.start();
--- packages/asl-workflow/lib/messager/queue/index.js
+++ packages/asl-workflow/lib/messager/queue/index.js
@@ -1,23 +1,31 @@
-const AWS = require('aws-sdk');
+const { SQSClient, SendMessageCommand } = require('@aws-sdk/client-sqs');
 
 module.exports = settings => {
-  const sqs = new AWS.SQS({
-    apiVersion: '2012-11-05',
+  const configParams = {
     region: settings.region,
     accessKeyId: settings.accessKey,
     secretAccessKey: settings.secret
-  });
+  };
+  const isLocal = settings.url?.includes('localhost') || settings.url?.includes('localstack');
+  if (isLocal) {
+    configParams.endpoint = new URL(settings.url).origin;
+    configParams.sslEnabled = false;
+  }
 
-  return key => {
+  // Create SQS client using AWS SDK v3
+  const sqs = new SQSClient(configParams);
+
+  return async key => {
     const params = {
       QueueUrl: settings.url,
       MessageBody: JSON.stringify({ key })
     };
-    return new Promise((resolve, reject) => {
-      sqs.sendMessage(params, (err, response) => {
-        return err ? reject(err) : resolve(response);
-      });
-    });
 
+    try {
+      return await sqs.send(new SendMessageCommand(params));
+    } catch (err) {
+      console.log(`Error Sending message to SQS queue: ${err}`);
+      throw err;
+    }
   };
 };
