e865b878568d6727c8de912817e19a7d2103f23c
Parvin <parvin.baishkiyar@digital.homeoffice.gov.uk>
2026-02-13 12:14:39 +0000

    asl-5020-Update-asl-data-exports-to-use-aws-sdk-v3

--- packages/asl-data-exports/lib/clients/s3-upload.js
+++ packages/asl-data-exports/lib/clients/s3-upload.js
@@ -1,23 +1,23 @@
 const { S3 } = require('@asl/service/clients');
+const { Upload } = require('@aws-sdk/lib-storage');
+const { Readable } = require('stream');
 
 module.exports = settings => {
   const s3Client = S3({ s3: settings });
 
-  const upload = ({ key, stream }) => {
-    const params = {
-      Bucket: settings.bucket,
-      Body: stream,
-      Key: key,
-      ServerSideEncryption: settings.kms ? 'aws:kms' : undefined,
-      SSEKMSKeyId: settings.kms
-    };
-
-    return new Promise((resolve, reject) => {
-      s3Client.upload(params, (err, result) => {
-        err ? reject(err) : resolve(result);
-      });
+  const upload = async ({ key, stream }) => {
+    const realStream = Readable.from(stream);
+    const uploader = new Upload({
+      client: s3Client,
+      params: {
+        Bucket: settings.bucket,
+        Key: key,
+        Body: realStream,
+        ServerSideEncryption: settings.kms ? 'aws:kms' : undefined,
+        SSEKMSKeyId: settings.kms
+      }
     });
+    return uploader.done();
   };
-
   return upload;
 };
