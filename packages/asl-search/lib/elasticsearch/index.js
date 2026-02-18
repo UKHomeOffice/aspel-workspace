443ba409729596fe550c04a2dcf648ea201cd7fa
Parvin <parvin.baishkiyar@digital.homeoffice.gov.uk>
2026-02-18 13:09:36 +0000

    old concept of createESConnection, does not work, as many classes/methods are not exposed. We need to use the recommended approach of createAwsSigV4Transport

--- packages/asl-search/lib/elasticsearch/index.js
+++ packages/asl-search/lib/elasticsearch/index.js
@@ -1,46 +1,62 @@
-const { Client } = require('@elastic/elasticsearch');
-const { Connection } = require('@elastic/elasticsearch');
+const { Client, Transport } = require('@elastic/elasticsearch');
 const { request } = require('http');
 const { sign } = require('aws4');
 
 /**
- * Replacement for createAWSConnection (acuris)
- * Implements AWS SigV4 signing using explicit credentials
+ * Creates a custom AWS SigV4 Transport class
+ * This is the ONLY supported extension point in ES v8
  */
-const createESConnection = (credentials) =>
-  class AWSConnection extends Connection {
-    constructor(opts) {
-      super(opts);
+function createAwsSigV4Transport(credentials) {
+  return class AwsSigV4Transport extends Transport {
+    request(params, options, callback) {
+      const signed = sign(
+        {
+          host: params.hostname,
+          path: params.path,
+          method: params.method,
+          headers: params.headers,
+          body: params.body,
+          service: 'es',
+          region: credentials.region
+        },
+        {
+          accessKeyId: credentials.key,
+          secretAccessKey: credentials.secret
+        }
+      );
 
-      this.makeRequest = (reqParams) => {
-        const signed = sign(
-          {
-            ...reqParams,
-            service: 'es',
-            region: credentials.region
-          },
-          {
-            accessKeyId: credentials.key,
-            secretAccessKey: credentials.secret
-          }
-        );
+      const req = request(signed, (res) => {
+        callback(null, {
+          statusCode: res.statusCode,
+          headers: res.headers,
+          body: res
+        });
+      });
 
-        return request(signed);
-      };
+      if (params.body) req.write(params.body);
+      req.end();
     }
   };
+}
 
+/**
+ * Main ES client factory
+ */
 const createESClient = async (options) => {
   if (options.aws?.credentials?.key) {
+    console.log('creating AWS client');
+
     const credentials = {
       key: options.aws.credentials.key,
       secret: options.aws.credentials.secret,
       region: options.aws.region
     };
 
+    const AwsTransport = createAwsSigV4Transport(credentials);
+
     return new Client({
       ...options.aws.client,
-      Connection: createESConnection(credentials)
+      Transport: AwsTransport
     });
   }
 
