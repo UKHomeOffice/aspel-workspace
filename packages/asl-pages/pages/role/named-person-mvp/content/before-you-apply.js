7d1cc378e72f202c4bba8b01acaf1fd318c782b9
kiran-varma-home-office <Kiran.Varma@digital.homeoffice.gov.uk>
2026-03-23 12:21:09 +0000

    fix: update email addresses and improve mandatory training messages

--- packages/asl-pages/pages/role/named-person-mvp/content/before-you-apply.js
+++ packages/asl-pages/pages/role/named-person-mvp/content/before-you-apply.js
@@ -23,7 +23,7 @@ You must ensure:
 * the Named Training and Competency Officer (NTCO) has endorsed their training and exemptions:
   * the nominee has discussed their training and exemptions with the NTCO
   * the NTCO has checked their mandatory training certificates
-  * the NTCO has checked any certificates and evidence to support exemption requests, and emailed them to ASRU Licensing: [ASRULicensing@homeoffice.gov.uk](mailto:ASRULicensing@homeoffice.gov.uk)
+  * the NTCO has checked any certificates and evidence to support exemption requests, and emailed them to ASRU Licensing: [asrulicensing@homeoffice.gov.uk](mailto:asrulicensing@homeoffice.gov.uk)
 * they have updated their training and exemptions in their [training record in ASPeL]({{trainingDashboardUrl}})
 
 ### Skills and experience
@@ -83,7 +83,7 @@ module.exports = merge({}, baseContent, {
 * they have agreed to be nominated
 * the legally accountable person supports the nomination
 * you can describe why they are suitable for the role
-* they have no significant conflict of interest, and you have sent the declaration form to ASRU Licensing: [ASRULicensing@homeoffice.gov.uk](mailto:ASRULicensing@homeoffice.gov.uk)
+* they have no significant conflict of interest, and you have sent the declaration form to ASRU Licensing: [asrulicensing@homeoffice.gov.uk](mailto:asrulicensing@homeoffice.gov.uk)
 * you have added them as an ASPeL user`
     },
     PELH: {
@@ -92,7 +92,7 @@ module.exports = merge({}, baseContent, {
 ### Before you nominate someone for the PEL holder role you must ensure:
 
 * you can describe why they are suitable for the role
-* they have no significant conflict of interest, and you have sent the declaration form to ASRU Licensing: [ASRULicensing@homeoffice.gov.uk](mailto:ASRULicensing@homeoffice.gov.uk)`
+* they have no significant conflict of interest, and you have sent the declaration form to ASRU Licensing: [asrulicensing@homeoffice.gov.uk](mailto:asrulicensing@homeoffice.gov.uk)`
     },
     HOLC: {
       title: 'Before you nominate someone for a HOLC role',
