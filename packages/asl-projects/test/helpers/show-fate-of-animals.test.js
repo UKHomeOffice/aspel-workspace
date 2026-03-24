f81ee52c662ab1a00a4b5c2bf6eb574f88894069
farrukhishaq <farrukh.ishaq@homeoffice.gov.uk>
2026-03-24 14:00:39 +0000

    tested showFateOfAnimals.

--- packages/asl-projects/test/helpers/show-fate-of-animals.test.js
+++ packages/asl-projects/test/helpers/show-fate-of-animals.test.js
@@ -3,10 +3,16 @@ const { showFateOfAnimals } = require('../../client/helpers/show-fate-of-animals
 
 describe('helpers/fate-of-animals', () => {
 
-  const buildValues = (fates) => ({
-    isStandardProtocol: true,
-    standardProtocolType: 'standard',
-    protocolName: 'rodent-breeding-mild',
+  // Flexible builder for test values
+  const buildValues = ({
+                         fates = [],
+                         isStandardProtocol = true,
+                         standardProtocolType = 'standard',
+                         protocolName = 'rodent-breeding-mild'
+                       } = {}) => ({
+    isStandardProtocol,
+    standardProtocolType,
+    protocolName,
     project: {
       'fate-of-animals': fates
     }
@@ -14,14 +20,10 @@ describe('helpers/fate-of-animals', () => {
 
   describe('showFateOfAnimals', () => {
 
-    describe('when any main fate is selected (hasAllFates)', () => {
+    describe('standard rodent-breeding-mild protocol', () => {
 
-      it('should allow only main fates and hide set-free & rehomed', () => {
-        const values = buildValues([
-          'killed',
-          'set-free',
-          'rehomed'
-        ]);
+      it('should allow main fates and hide set-free & rehomed when hasAllFates', () => {
+        const values = buildValues({ fates: ['killed', 'set-free', 'rehomed'] });
 
         assert.strictEqual(showFateOfAnimals(values, 'killed'), true);
         assert.strictEqual(showFateOfAnimals(values, 'continued-use'), true);
@@ -32,16 +34,8 @@ describe('helpers/fate-of-animals', () => {
         assert.strictEqual(showFateOfAnimals(values, 'rehomed'), false);
       });
 
-    });
-
-    describe('when only limited fates are selected', () => {
-
-      it('should allow only continued-use and kept-alive', () => {
-        const values = buildValues([
-          'set-free',
-          'rehomed',
-          'kept-alive'
-        ]);
+      it('should allow only continued-use and kept-alive when limited fates selected', () => {
+        const values = buildValues({ fates: ['set-free', 'rehomed', 'kept-alive'] });
 
         assert.strictEqual(showFateOfAnimals(values, 'continued-use'), true);
         assert.strictEqual(showFateOfAnimals(values, 'kept-alive'), true);
@@ -50,6 +44,54 @@ describe('helpers/fate-of-animals', () => {
         assert.strictEqual(showFateOfAnimals(values, 'continued-use-2'), false);
         assert.strictEqual(showFateOfAnimals(values, 'set-free'), false);
         assert.strictEqual(showFateOfAnimals(values, 'rehomed'), false);
+      });
+
+    });
+
+    describe('non-standard protocol scenarios', () => {
+
+      it('should return true for all fields if isStandardProtocol is false', () => {
+        const values = buildValues({
+          fates: ['killed'],
+          isStandardProtocol: false,
+          standardProtocolType: 'standard',
+          protocolName: 'rodent-breeding-mild'
+        });
+
+        const fields = [
+          'killed',
+          'continued-use',
+          'continued-use-2',
+          'set-free',
+          'rehomed',
+          'kept-alive'
+        ];
+
+        fields.forEach(field => {
+          assert.strictEqual(showFateOfAnimals(values, field), true);
+        });
+      });
+
+      it('should return true for all fields if standardProtocolType is editable', () => {
+        const values = buildValues({
+          fates: ['killed'],
+          isStandardProtocol: true,
+          standardProtocolType: 'editable',
+          protocolName: 'rodent-breeding-mild'
+        });
+
+        const fields = [
+          'killed',
+          'continued-use',
+          'continued-use-2',
+          'set-free',
+          'rehomed',
+          'kept-alive'
+        ];
+
+        fields.forEach(field => {
+          assert.strictEqual(showFateOfAnimals(values, field), true);
+        });
       });
 
     });
