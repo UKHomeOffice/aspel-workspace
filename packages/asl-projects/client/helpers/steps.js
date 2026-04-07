e7b22f29c6c3da2a806593a4f9325dc7561e1b39
farrukhishaq <farrukh.ishaq@homeoffice.gov.uk>
2026-04-07 12:56:18 +0100

    general cleanup

--- packages/asl-projects/client/helpers/steps.js
+++ packages/asl-projects/client/helpers/steps.js
@@ -106,4 +106,3 @@ export const reusableStepFieldKeys = (protocol) => {
 export const getRepeatedFromProtocolIndex = (step, currentProtocolId) => {
   return (step.usedInProtocols || []).length > 0 && step.usedInProtocols[0].protocolId !== currentProtocolId ? step.usedInProtocols[0].protocolNumber : undefined;
 };
-
