f50452a8f644ef92ae78e42c1f430f92998a88c2
kiran-varma-home-office <Kiran.Varma@digital.homeoffice.gov.uk>
2026-04-08 16:42:57 +0100

    feat: implement incomplete training module and related routing

---
+++ packages/asl-pages/pages/role/named-person-mvp/routers/incomplete-training.js
@@ -0,0 +1,43 @@
+const { form } = require('../../../common/routers');
+const { Router } = require('express');
+const schema = require('../schema').incompleteTraining;
+const { format, parse, isValid } = require('date-fns');
+
+module.exports = ({ formId }) => {
+  const app = Router({ mergeParams: true });
+
+  app.use(
+    form({
+      configure(req, res, next) {
+        const roleType = req.session.form[formId].values.type;
+        req.form.schema = schema(roleType);
+        next();
+      },
+      process: (req, res, next) => {
+        const day = req.body['completeDate-day'];
+        const month = req.body['completeDate-month'];
+        const year = req.body['completeDate-year'];
+
+        const completeDate = parse(`${year}-${month}-${day}`, 'yyyy-M-d', new Date());
+        req.form.values.completeDate = isValid(completeDate) ? format(completeDate, 'yyyy-MM-dd') : '';
+        next();
+      },
+      locals: (req, res, next) => {
+        Object.assign(res.locals.static, {
+          roleType: req.session.form[formId].values.type.toUpperCase()
+        });
+        next();
+      },
+      saveValues: (req, res, next) => {
+        req.session.form[formId].values = req.form.values;
+        next();
+      }
+    })
+  );
+
+  app.post('/', (req, res, next) => {
+    return res.redirect(req.buildRoute('role.namedPersonMvp.confirm'));
+  });
+
+  return app;
+};
--- packages/asl-pages/pages/role/named-person-mvp/routers/index.js
+++ packages/asl-pages/pages/role/named-person-mvp/routers/index.js
@@ -2,5 +2,8 @@
 module.exports = {
   selectRole: require('./select-role'),
   beforeYouApply: require('./before-you-apply'),
-  mandatoryTraining: require('./mandatory-training')
+  mandatoryTraining: require('./mandatory-training'),
+  incompleteTraining: require('./incomplete-training')
+  // confirm: require('./confirm'),
+  // success: require('./success')
 };
