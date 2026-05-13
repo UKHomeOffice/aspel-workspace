5429a49d040aa738b4bc1448ce4dbbc88678dce6
Parvin <parvin.baishkiyar@digital.homeoffice.gov.uk>
2026-05-13 21:08:45 +0100

    13may21

--- packages/asl-projects/test/specs/client/helpers/has-existing-data-for-checkbox.test.js
+++ packages/asl-projects/test/specs/client/helpers/has-existing-data-for-checkbox.test.js
@@ -30,7 +30,7 @@ const emptyProject = {
     {
       'method-and-justification': cloneDeep(emptyTextArea),
       'continued-use-relevant-project': cloneDeep(emptyTextArea),
-      'continued-use-2': true
+      'continued-use-2': false
     }
   ],
   'keeping-alive-complete': false,
