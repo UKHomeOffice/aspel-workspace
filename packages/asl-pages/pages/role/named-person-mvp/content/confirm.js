07e18c4659199d5e747ed822e712972d13eeb462
kiran-varma-home-office <Kiran.Varma@digital.homeoffice.gov.uk>
2026-06-04 22:32:49 +0100

    feat: simplify NACWO nomination confirmation text and structure

--- packages/asl-pages/pages/role/named-person-mvp/content/confirm.js
+++ packages/asl-pages/pages/role/named-person-mvp/content/confirm.js
@@ -40,18 +40,11 @@ module.exports = merge({}, baseContent, skillsAndExperienceContent, {
   },
   declarations: {
     nacwo: `\
-**By submitting this application, I confirm that {{profile.firstName}} has agreed to be nominated for the NACWO role.**
+**By submitting this application, I confirm that:**
 
-**I confirm that the NTCO has endorsed {{profile.firstName}}'s training and exemptions.**
-
-**I also confirm that the PEL holder supports the application and is confident that:**
-
-* {{profile.firstName}} has the following recommended skills and experience:
-    * suitable expertise and training to minimise suffering and optimise the welfare of animals they are responsible for (this is covered by mandatory training modules PILA, L and E1)
-    * appropriate personal and managerial authority to promote high standards - for example, they are managing a service, area or team of animal technicians
-    * good communication and diplomacy skills to champion a culture of care among both scientific and husbandry staff
-
-* {{profile.firstName}} has no significant conflict of interest, and their declaration form is held on record at the establishment`,
+  * {{profile.firstName}} has agreed to be nominated for the NACWO role
+  * the NTCO has endorsed {{profile.firstName}}'s training and exemptions
+  * the PEL holder (or legally accountable person) supports the nomination and is confident that {{profile.firstName}} has no significant conflict of interest, and their declaration form is held on record at the establishment`,
     nvs: `\
 **By submitting this application, I confirm that {{profile.firstName}} has agreed to be nominated for the NVS role.**
 
