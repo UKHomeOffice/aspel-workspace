9620069b1c82c1861b816c5136e291aa2a4a329f
Ishaq <156688098+farrukhishaq@users.noreply.github.com>
2026-07-23 14:02:08 +0100

    retain experimental, standard protocol hint.

--- packages/asl-projects/client/schema/v1/protocols/fate.js
+++ packages/asl-projects/client/schema/v1/protocols/fate.js
@@ -1,5 +1,10 @@
 import { markdownLink, getCurrentURLForFateOfAnimals } from '../../../helpers';
 import NTSFateOfAnimalFields from '../../../helpers/nts-field';
+import { withProtocolContext } from './helpers/with-protocol-context';
+
+const baseHint = () => `Select all that apply. These options are based on what you selected in the ${markdownLink('non-technical summary', getCurrentURLForFateOfAnimals())}.`;
+
+const standardProtocolHint = () => `Select all that apply. These options are based on what you selected in the ${markdownLink('non-technical summary', getCurrentURLForFateOfAnimals())} and what is permitted for this standard protocol.`;
 
 export default {
   title: 'Fate of animals',
@@ -8,15 +13,25 @@ export default {
     order: 11
   },
   fields: [
-    {
-      name: 'fate',
-      label: 'What will happen to animals at the end of this protocol?',
-      hint: () => `Select all that apply. These options are based on what you selected in the ${markdownLink('non-technical summary', getCurrentURLForFateOfAnimals())} and what is permitted for this standard protocol.`,
-      type: 'checkbox',
-      preserveHierarchy: true,
-      className: 'smaller',
-      options: Object.values(NTSFateOfAnimalFields())
-    }
+    withProtocolContext(
+      {
+        name: 'fate',
+        label: 'What will happen to animals at the end of this protocol?',
+        hint: baseHint,
+        type: 'checkbox',
+        preserveHierarchy: true,
+        className: 'smaller',
+        options: Object.values(NTSFateOfAnimalFields())
+      },
+      {
+        editable: {
+          hint: standardProtocolHint
+        },
+        standard: {
+          hint: standardProtocolHint
+        }
+      }
+    )
   ]
 };
 
