ca2bac2dc92f36f3157bc595a19497f0133cc83a
Kiran Varma <Kiran.Varma@digital.homeoffice.gov.uk>
2026-08-10 16:36:20 +0100

    fix: [ASL-4985] NPRC declaration missing (#471)
    * Fix CSS class naming for date range input fields to maintain consistency
    
    * Add CVE-2025-71329 and CVE-2025-71330 to exceptions list
    
    * Update confirmation text for NPRC role application to clarify conflict of interest management

--- packages/asl-pages/pages/role/named-person-mvp/content/confirm.js
+++ packages/asl-pages/pages/role/named-person-mvp/content/confirm.js
@@ -73,6 +73,12 @@ module.exports = merge({}, baseContent, skillsAndExperienceContent, {
   * the NTCO has endorsed {{profile.firstName}}'s training and exemptions
   * the PEL holder (or NPRC) supports the nomination and is satisfied that the nominee has no conflicts of interest, or that any conflicts are adequately managed
   * their declaration form is held on record at the establishment`,
+    nprc: `\
+**By submitting this application, I confirm that:**
+
+  * {{profile.firstName}} {{profile.lastName}} has agreed to be nominated for the NPRC role
+  * the legally accountable person supports the nomination and is satisfied that the nominee has no conflicts of interest, or that any conflicts are adequately managed
+  * the declaration form has been sent to ASRU Licensing`,
     default: `\
 **By submitting this application, I confirm that:**
 
