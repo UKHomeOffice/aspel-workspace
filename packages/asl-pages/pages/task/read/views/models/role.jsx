72b35e776338c0beda497863754c0132cef0e6c4
kiran-varma-home-office <Kiran.Varma@digital.homeoffice.gov.uk>
2026-09-10 10:32:55 +0100

    fix: update role heading snippet logic for PELH and NPRC roles

--- packages/asl-pages/pages/task/read/views/models/role.jsx
+++ packages/asl-pages/pages/task/read/views/models/role.jsx
@@ -26,7 +26,7 @@ export default function Role({ task, values, schema }) {
   const isHolcRole = roleType === 'holc';
   const isPelHOrNprcRole = ['pelh', 'nprc'].includes(roleType);
   const namedPersonFeatureFlag = useFeatureFlag(FEATURE_FLAG_NAMED_PERSON_MVP);
-  const roleHeadingSnippet = isPelHOrNprcRole || (isNamedPersonCreate && ((namedPersonFeatureFlag || isNamedPersonVersion) && !isHolcRole))
+  const roleHeadingSnippet = isNamedPersonCreate && ((namedPersonFeatureFlag || isNamedPersonVersion) && !isHolcRole)
     ? 'sticky-nav.roleApplication'
     : 'sticky-nav.role';
 
