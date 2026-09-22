ce01378b519257a2ee56b80d0255068e9229933a
Mokter Hossain <mokter.hossain@digital.homeoffice.gov.uk>
2026-09-22 12:42:37 +0100

    changes

--- packages/asl-components/src/training-summary/index.jsx
+++ packages/asl-components/src/training-summary/index.jsx
@@ -46,9 +46,9 @@ function Row({ certificate, actions, basePage }) {
                     certificate.isExemption
                         ? (
                             <>
-                              <p>
-                                <span>Added on: </span><span>{certificate.createdAt ? format(certificate.createdAt, dateFormat) : '-'}</span>
-                              </p>
+                                <p>
+                                    <span>Added on: </span><span>{certificate.createdAt ? format(certificate.createdAt, dateFormat) : '-'}</span>
+                                </p>
                                 <p className="preserve-whitespace">{certificate.exemptionReason}</p>
                             </>
                         )
